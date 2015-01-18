ed7cf55c3109a03e6743ed9a7bbcd15b5414f906
Paul Walsh <paulywalsh@gmail.com>
2015-01-18 14:37:07 +0200

    Prevent import errors from stuff I haven't commited yet

--- tabular_validator/validators/__init__.py
+++ tabular_validator/validators/__init__.py
@@ -1,6 +1,8 @@
 from .base import Validator
 from .spec import SpecValidator
 from .structure import StructureValidator
+from .schema import SchemaValidator
 
 
-__all__ = ['Validator', 'SpecValidator', 'StructureValidator']
+__all__ = ['Validator', 'SpecValidator', 'StructureValidator',
+           'SchemaValidator']
---
+++ tabular_validator/validators/schema.py
@@ -0,0 +1,6 @@
+from . import base
+
+
+class SchemaValidator(base.Validator):
+
+    name = 'schema'
