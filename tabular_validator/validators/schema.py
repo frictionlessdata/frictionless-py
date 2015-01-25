c25b0faf13488a719632c031d1247c885d715710
Paul Walsh <paulywalsh@gmail.com>
2015-01-25 11:13:41 +0200

    WIP Schema Validator (untested)

--- tabular_validator/validators/schema.py
+++ tabular_validator/validators/schema.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 
 from . import base
 from .. import utilities
+from .. import compat
 
 
 class SchemaValidator(base.Validator):
@@ -37,7 +38,7 @@ class SchemaValidator(base.Validator):
                     'category': 'headers',
                     'level': 'error',
                     'position': None,
-                    'message': 'The data fields do not match the schema'
+                    'message': 'The headers do not match the schema'
                 })
 
         else:
@@ -48,7 +49,7 @@ class SchemaValidator(base.Validator):
                     'category': 'headers',
                     'level': 'error',
                     'position': None,
-                    'message': ''
+                    'message': 'The headers do not match the schema'
                 })
 
         return valid, headers
@@ -57,20 +58,31 @@ class SchemaValidator(base.Validator):
 
         valid = True
 
-        # NOT YET safe if headers repeat - need to build dict differently
         if not len(headers) == len(row):
             valid = False
-            # REPORT
+            self.report.write({
+                'name': 'Incorrect Dimensions',
+                'category': 'row',
+                'level': 'error',
+                'position': index,
+                'message': 'The row does not match the header dimensions.'
+            })
 
         else:
-            obj = {k: v for k, v in zip(headers, row)}
-
             for k, v in zip(headers, row):
                 # check type and format
                 if not isinstance(v, self.schema.type(k)):
                     valid = False
-                    # REPORT
-                    # DO FORMAT
+                    self.report.write({
+                        'name': 'Incorrect type',
+                        'category': 'row',
+                        'level': 'error',
+                        'position': index,
+                        'message': ('The cell {0} is of the wrong '
+                                    'type'.format(k))
+                    })
+
+                #TODO: Check format
 
                 # CONSTRAINTS
                 constraints = self.schema.constraints('k')
@@ -78,20 +90,18 @@ class SchemaValidator(base.Validator):
                     # check constraints.required
                     if constraints.get('required') and not v:
                         valid = False
-                        # REPORT
+                        self.report.write({
+                            'name': 'Missing required field',
+                            'category': 'row',
+                            'level': 'error',
+                            'position': index,
+                            'message': ('The cell {0} has no value, but '
+                                        'is required'.format(k))
+                        })
 
-                    # TODO: check constraints.unique
+                # TODO: check constraints.unique
 
                 # TODO: check constraints.min* and constraints.max*
-
-                # PRIMARY KEY
-                primaryKey = self.schema.primaryKey()
-                if primaryKey:
-                    if isinstance(primaryKey, str):
-                        pass
-                # IF PK in spec, check if fields are valid
-
-                # FOREIGN KEYS
 
         if self.transform:
             pass
@@ -114,10 +124,6 @@ class JSONTableSchema(object):
             raise InvalidJSONTableSchemaError
 
         self.as_json = json.dumps(self.as_python)
-
-    @classmethod
-    def validateschema(cls, schema):
-        return True
 
     def to_python(self):
 
@@ -148,4 +154,199 @@ class JSONTableSchema(object):
 
     def constraints(self, field_name):
         return self.field(field_name).get('constraints')
-    
+
+    @classmethod
+    def validateschema(cls, schema):
+        """Validate as JSON Table Schema based on RFC 1.0.pre-4.
+
+        This is a bit ugly. Aims to provide complete spec coverage.
+        Plan to clean up and pull put into own module.
+
+        """
+
+        _validation_errors = []
+
+        # a schema is a hash
+        if not isinstance(schema, dict):
+            _validation_errors.append('The schema is not a hash')
+
+        # which MUST contain a key `fields`
+        if not schema.get('fields'):
+            _validation_errors.append('does not contain a `fields` key')
+
+        # `fields` MUST be an array
+        if not isinstance(schema.get('fields'), list):
+            _validation_errors.append('The `fields` key is present but is not an array')
+
+        # each entry in the `fields` array MUST be a hash
+        if not all([isinstance(o, dict) for o in schema['fields']]):
+            _validation_errors.append('One or more of the descriptors in `fields` is not a hash')
+
+        # each entry in the `fields` array MUST have a `name` key
+        if not all([o.get('name') for o in schema['fields']]):
+            _validation_errors.append('One or more of the descriptors in `fields` does not have a `name` key')
+
+        # each entry in the `fields` array MAY have a `constraints` key
+        # if `constraints` is present, then `constraints` MUST be a hash
+        if not all([isinstance(o['constraints'], dict) for o in
+                   schema['fields'] if o.get('constraints')]):
+            _validation_errors.append('One or more of the descriptors in `fields` has a `constraints` key which is not a hash')
+
+        # constraints may contain certain keys (each has a specific meaning)
+        for constraints, _type in [(o['constraints'], o.get('type')) for o in
+                                   schema['fields'] if
+                                   o.get('constraints')]:
+
+            # IF `required` key, then it is a boolean
+            if constraints.get('required') and not \
+                    isinstance(constraints['required'], bool):
+                _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `required` key which is not boolean')
+
+            # IF `minLength` key, then it is an integer
+            if constraints.get('minLength') and not \
+                    isinstance(constraints['minLength'], int):
+                _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `minLength` key which is not an integer')
+
+            # IF `maxLength` key, then it is an integer
+            if constraints.get('maxLength') and not \
+                    isinstance(constraints['maxLength'], int):
+                _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `maxLength` key which is not an integer')
+
+            # IF `unique` key, then it is a boolean
+            if constraints.get('unique') and not \
+                    isinstance(constraints['unique'], bool):
+                _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `unique` key which is not boolean')
+
+            # IF `pattern` key, then it is a regex
+            if constraints.get('pattern') and not \
+                    isinstance(constraints['pattern'], compat.str):
+                _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `pattern` key which is not a string')
+
+            # IF `minimum` key, then it DEPENDS on `field` TYPE
+            if constraints.get('minimum'):
+
+                # IF `type` is integer
+                if isinstance(_type, int) and not \
+                        isinstance(constraints['minimum'], int):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `minimum` key with a mismatched type to its associated field')
+
+                # IF `type` is date
+                elif isinstance(_type, datetime.date) and not \
+                        isinstance(constraints['minimum'], datetime.date):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `minimum` key with a mismatched type to its associated field')
+
+                # IF `type` is time
+                elif isinstance(_type, datetime.time) and not \
+                        isinstance(constraints['minimum'], datetime.time):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `minimum` key with a mismatched type to its associated field')
+
+                # IF `type` is datetime
+                elif isinstance(_type, datetime.datetime) and not \
+                        isinstance(constraints['minimum'], datetime.datetime):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `minimum` key with a mismatched type to its associated field')
+
+                else:
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `minimum` key with unclear application to its field')
+
+            # IF `maximum` key, then it DEPENDS on `field` TYPE
+            if constraints.get('maximum'):
+
+                # IF `type` is integer
+                if isinstance(_type, int) and not \
+                        isinstance(constraints['maximum'], int):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `maximum` key with a mismatched type to its associated field')
+
+                # IF `type` is date
+                elif isinstance(_type, datetime.date) and not \
+                        isinstance(constraints['maximum'], datetime.date):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `maximum` key with a mismatched type to its associated field')
+
+                # IF `type` is time
+                elif isinstance(_type, datetime.time) and not \
+                        isinstance(constraints['maximum'], datetime.time):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `maximum` key with a mismatched type to its associated field')
+
+                # IF `type` is datetime
+                elif isinstance(_type, datetime.datetime) and not \
+                        isinstance(constraints['maximum'], datetime.datetime):
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `maximum` key with a mismatched type to its associated field')
+
+                else:
+                    _validation_errors.append('A `constraints` descriptor in one or more field descriptors has a `maximum` key with unclear application to its field')
+
+        # the hash MAY contain a key `primaryKey`
+        if schema.get('primaryKey'):
+
+            # `primaryKey` MUST be a string or an array
+            if not isinstance(schema['primaryKey'], (compat.str, list)):
+                _validation_errors.append('A `primaryKey` key is present but it is not an array or string')
+
+            # ensure that the primary key matches field names
+            if isinstance(schema['primaryKey'], compat.str) and \
+                    not schema['primaryKey'] in \
+                        [f['name'] for f in schema['fields']]:
+                _validation_errors.append('The `primaryKey` does not match a name in any field descriptor')
+
+            else:
+                for k in schema['primaryKey']:
+                    if not k in [f['name'] for f in schema['fields']]:
+                        _validation_errors.append('A key in `primaryKey` does not match a name in any field descriptor')
+
+        # the hash may contain a key `foreignKeys`
+        if schema.get('foreignKeys'):
+
+            # `foreignKeys` MUST be an array
+            if not isinstance(schema['foreignKeys'], list):
+                _validation_errors.append('A `foreignKeys` key is present but it is not an array')
+
+            # each `foreignKey` in `foreignKeys` MUST be a hash
+            if not all([isinstance(o, dict) for o in
+                       schema['foreignKeys']]):
+                _validation_errors.append('At least one `foreignKey` in `foreignKeys` key is not a hash')
+
+            # each `foreignKey` in `foreignKeys` MUST have a `fields` key
+            if not all([o.get('fields') for o in
+                       schema['foreignKeys']]):
+                _validation_errors.append('At least one `foreignKey` in `foreignKeys` key does not have a `fields` key')
+
+            # each `fields` key in a `foreignKey` MUST be a string or array
+            if not all([isinstance(o.get('fields'), (compat.str, list))
+                       for o in schema['foreignKeys']]):
+                _validation_errors.append('At least one `foreignKey` in `foreignKeys` key has a `fields` key that is not a string or array')
+
+            for fk in schema['foreignKeys']:
+
+                # ensure that `foreignKey.fields` match field names
+                if isinstance(fk.get('fields'), compat.str):
+                    if fk.get('fields') not in [f['name'] for f in
+                                                schema['fields']]:
+                        _validation_errors.append('A `foreignKey.fields` argument does not match a name in any field descriptor')
+
+                else:
+                    for field in fk.get('fields'):
+                        if not field in [f['name'] for f in
+                                         schema['fields']]:
+                            _validation_errors.append('A key in a `foreignKey.fields` does not match a name in any field descriptor')
+
+                # ensure that `foreignKey.reference` is present and is a hash
+                if not isinstance(fk.get('reference'), dict):
+                    _validation_errors.append('At least one `foreignKey` is missing a `reference` key, or, the `reference` key is not a hash')
+
+                # ensure that `foreignKey.reference` has a `resource` key
+                if not 'resource' in fk.get('reference', {}):
+                    _validation_errors.append('At least one `foreignKey.reference` is missing a `resource` key')
+
+                # ensure that `foreignKey.reference` has a `fields` key
+                if not 'fields' in fk.get('reference', {}):
+                    _validation_errors.append('At least one `foreignKey.reference` is missing a `fields` key')
+
+                # ensure that `foreignKey.reference.fields`
+                # matches outer `fields`
+                if isinstance(fk.get('fields'), compat.str):
+                    if not isinstance(fk['reference']['fields'], compat.str):
+                        _validation_errors.append('Reference fields do match match the fields type')
+                else:
+                    if not len(fk.get('fields')) == len(fk['reference']['fields']):
+                        _validation_errors.append('Reference fields do match match the fields array length')
+
+        return not bool(_validation_errors)
