d7e30a23c3b1d5709e11540033dc0eabe3217832
Paul Walsh <paulywalsh@gmail.com>
2015-02-20 12:21:31 +0200

    bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name='tabular-validator',
     description='A toolkit for validating tabular data through a pipeline.',
     long_description='A toolkit for validating tabular data through a pipeline.',
-    version='0.1.1',
+    version='0.1.2',
     author='Open Knowledge Foundation',
     author_email='info@okfn.org',
     url='http://okfn.org',
