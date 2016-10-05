48c2ff0fa2c13cff9a6acf9ce075500ead400af0
roll <roll@post.agency>
2016-10-05 17:39:53 +0200

    added dataset check stubs

---
+++ goodtables/checks/dataset/jsontableschema_error.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+# Module API
+
+def jsontableschema_error(exception):
+    return []
