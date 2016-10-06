87609fbe42e2245832177415c61fcfd1ef7f26c0
roll <roll@post.agency>
2016-10-06 21:09:21 +0200

    fixed extra_header

--- goodtables/checks/head/extra_header.py
+++ goodtables/checks/head/extra_header.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+from copy import copy
 from jsontableschema import Schema, infer
 
 
@@ -11,20 +12,22 @@ from jsontableschema import Schema, infer
 
 def extra_header(columns, sample, infer_fields=False):
     errors = []
-    for column in columns:
+    for column in copy(columns):
         if 'field' not in column:
             errors.append({
                 'message': 'Extra header',
                 'row-number': None,
                 'col-number': column['number'],
             })
-            if infer_fields:
-                column_sample = []
-                for row in sample:
-                    value = None
-                    if len(row) > column['number']:
-                        value = row[column['number']]
-                    column_sample.append(value)
-                descriptor = infer([column['header']], column_sample)
-                column['field'] = Schema(descriptor).fields[0]
+            if not infer_fields:
+                columns.remove(column)
+                continue
+            column_sample = []
+            for row in sample:
+                value = None
+                if len(row) > column['number']:
+                    value = row[column['number']]
+                column_sample.append(value)
+            descriptor = infer([column['header']], column_sample)
+            column['field'] = Schema(descriptor).fields[0]
     return errors
