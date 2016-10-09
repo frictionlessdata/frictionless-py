90fffe5bc242fb8180c5bcb4a6517d58aa800bff
roll <roll@post.agency>
2016-10-09 19:34:15 +0200

    implemented proper non-matching-header without ordering

--- goodtables/checks/head/non_matching_header.py
+++ goodtables/checks/head/non_matching_header.py
@@ -12,19 +12,22 @@ from ...registry import check
 
 @check('non-matching-header')
 def non_matching_header(errors, columns, sample=None, order_fields=False):
-    headers = set(column['name'] for column in columns if 'name' in column)
-    field_names = set(column['field'].name for column in columns if 'field' in column)
-    for column in copy(columns):
-        header = column.get('header')
-        field_name = None
-        if 'field' in column:
-            field_name = column['field'].name
-        if header != field_name:
-            if header in field_names or field_name in headers:
-                errors.append({
-                    'code': 'non-matching-header',
-                    'message': 'Non matching header',
-                    'row-number': None,
-                    'column-number': column['number'],
-                })
-                columns.remove(column)
+
+    # No field ordering
+    if not order_fields:
+        for column in copy(columns):
+            if len(column) == 3:
+                if column['header'] != column['field'].name:
+                    # Add error
+                    errors.append({
+                        'code': 'non-matching-header',
+                        'message': 'Non matching header',
+                        'row-number': None,
+                        'column-number': column['number'],
+                    })
+                    # Remove column
+                    columns.remove(column)
+
+    # Field ordering
+    else:
+        raise NotImplementedError()
