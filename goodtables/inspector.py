3ca27d08c08a7f5acf2a42d2751daeeb94735947
roll <roll@post.agency>
2016-11-02 15:51:54 +0300

    fixed linting

--- goodtables/inspector.py
+++ goodtables/inspector.py
@@ -4,9 +4,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import io
-import os
-import json
 import inspect
 import datetime
 import operator
