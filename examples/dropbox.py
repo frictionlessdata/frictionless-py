c77873428094c6cc20534c056d210e9242f14c47
roll <eskarev@gmail.com>
2016-12-13 21:51:49 +0300

    simplified dropbox example

--- examples/dropbox.py
+++ examples/dropbox.py
@@ -3,31 +3,21 @@ from pprint import pprint
 from tabulator import Stream
 from goodtables import Inspector, preset
 
-# Create app to get key and secret at
-# https://www.dropbox.com/developers/apps
-# And add some csv files to FOLDER on dropbox
-APP_KEY = '<insert-app-key>'
-APP_SECRET = '<insert-app-secret>'
+# 1. Create app to generate access token at https://www.dropbox.com/developers/apps
+# 2. And add some csv files to FOLDER on dropbox
+ACCESS_TOKEN = '<insert-access-token>'
 FOLDER = '/goodtables'
 
-# Authorize clients
-flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
-authorize_url = flow.start()
-print('1. Go to: ' + authorize_url)
-print('2. Click "Allow" (you might have to log in first)')
-print('3. Copy the authorization code.')
-code = input("Enter the authorization code here: ").strip()
-access_token, user_id = flow.finish(code)
-client1 = dropbox.dropbox.Dropbox(access_token)
-client2 = dropbox.client.DropboxClient(access_token)
+# Get dropbox client
+client = dropbox.dropbox.Dropbox(ACCESS_TOKEN)
 
 @preset('dropbox')
 def dropbox_preset(source, **options):
     errors = []
     tables = []
-    for item in client2.metadata(source)['contents']:
-        if item['path'].endswith('.csv'):
-            url = client1.files_get_temporary_link(item['path']).link
+    for item in client.files_list_folder(source).entries:
+        if item.path_lower.endswith('.csv'):
+            url = client.files_get_temporary_link(item.path_lower).link
             tables.append({
                 'source': url,
                 'stream': Stream(url, headers=1, format='csv'),
