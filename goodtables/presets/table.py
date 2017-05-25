0353d35f8039e9b842a287ca21c7785726a6320e
roll <eskarev@gmail.com>
2017-05-25 16:45:52 +0300

    disallow datapackage inspection for table preset

--- goodtables/presets/table.py
+++ goodtables/presets/table.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import six
 import jsontableschema
 from tabulator import Stream
 from jsontableschema import Schema, validate
@@ -18,22 +19,28 @@ def table(source, schema=None, **options):
     warnings = []
     tables = []
 
+    # Ensure not a datapackage
+    if isinstance(source, six.string_types):
+        if source.endswith('datapackage.json'):
+            warnings.append('Use "datapackage" preset for Data Packages')
+
     # Prepare schema
-    if schema is not None:
-        # TODO: after tableschema-v1 will be ready
-        # - we should use Schema(strict=False) to handle schema errors on inspection
-        # - it means we don't need to validate schema here, mess with helpers etc
-        descriptor = schema
-        try:
-            from jsontableschema.helpers import load_json_source
-            loaded_descriptor = load_json_source(schema)
-            validate(loaded_descriptor, no_fail_fast=True)
-            schema = Schema(loaded_descriptor)
-        except jsontableschema.exceptions.MultipleInvalid as exception:
-            for error in exception.errors:
-                warnings.append(
-                    'Table schema "%s" has a validation error "%s"' %
-                    (schema, str(error).splitlines()[0]))
+    if not warnings:
+        if schema is not None:
+            # TODO: after tableschema-v1 will be ready
+            # - we should use Schema(strict=False) to handle schema errors on inspection
+            # - it means we don't need to validate schema here, mess with helpers etc
+            descriptor = schema
+            try:
+                from jsontableschema.helpers import load_json_source
+                loaded_descriptor = load_json_source(schema)
+                validate(loaded_descriptor, no_fail_fast=True)
+                schema = Schema(loaded_descriptor)
+            except jsontableschema.exceptions.MultipleInvalid as exception:
+                for error in exception.errors:
+                    warnings.append(
+                        'Table schema "%s" has a validation error "%s"' %
+                        (schema, str(error).splitlines()[0]))
 
     # Add table
     if not warnings:
