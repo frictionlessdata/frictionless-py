44dc14f4db0aed0e1f1a65a1f18ded9fdae31056
roll <eskarev@gmail.com>
2017-08-04 10:16:13 +0300

    renamed column to cells in non-matching-header

--- goodtables/checks/non_matching_header.py
+++ goodtables/checks/non_matching_header.py
@@ -20,63 +20,63 @@ class NonMatchingHeader(object):
     def __init__(self, order_fields=False, **options):
         self.__order_fields = order_fields
 
-    def check_headers(self, errors, columns, sample=None):
+    def check_headers(self, errors, cells, sample=None):
         if self.__order_fields:
-            _check_with_ordering(errors, columns)
+            _check_with_ordering(errors, cells)
         else:
-            _check_without_ordering(errors, columns)
+            _check_without_ordering(errors, cells)
 
 
 # Internal
 
 
-def _check_with_ordering(errors, columns):
+def _check_with_ordering(errors, cells):
     # Update fields order to maximally match headers order
-    headers = [column.get('header') for column in columns]
+    headers = [cell.get('header') for cell in cells]
     for index, header in enumerate(headers):
         if header is None:
             continue
-        field_name = _get_field_name(columns[index])
-        # Column header and field_name are different
+        field_name = _get_field_name(cells[index])
+        # Cell header and field_name are different
         if _slugify(header) != _slugify(field_name):
-            # If there is the match in next columns - swap fields
-            for column in columns[index+1:]:
+            # If there is the match in next cells - swap fields
+            for cell in cells[index+1:]:
                 # We've found field matching given header
-                if _slugify(header) == _slugify(_get_field_name(column)):
-                    _swap_fields(columns[index], column)
+                if _slugify(header) == _slugify(_get_field_name(cell)):
+                    _swap_fields(cells[index], cell)
                     break
                 if field_name:
                     # Given field matches some header also swap
-                    if _slugify(field_name) == _slugify(column.get('header')):
-                        _swap_fields(columns[index], column)
+                    if _slugify(field_name) == _slugify(cell.get('header')):
+                        _swap_fields(cells[index], cell)
     # Run check with no field ordering
-    _check_without_ordering(errors, columns)
+    _check_without_ordering(errors, cells)
 
 
-def _check_without_ordering(errors, columns):
-    for column in copy(columns):
-        if len(column) == 3:
-            if column['header'] != column['field'].name:
+def _check_without_ordering(errors, cells):
+    for cell in copy(cells):
+        if list(cell) == ['number', 'header', 'field']:
+            if cell['header'] != cell['field'].name:
                 # Add error
                 message = spec['errors']['non-matching-header']['message']
                 message = message.format(
-                    column_number=column['number'],
-                    field_name=column['field'].name)
+                    column_number=cell['number'],
+                    field_name=cell['field'].name)
                 errors.append({
                     'code': 'non-matching-header',
                     'message': message,
                     'row-number': None,
-                    'column-number': column['number'],
+                    'column-number': cell['number'],
                 })
-                if _slugify(column['header']) != _slugify(column['field'].name):
-                    # Remove column
-                    columns.remove(column)
+                if _slugify(cell['header']) != _slugify(cell['field'].name):
+                    # Remove cell
+                    cells.remove(cell)
 
 
-def _get_field_name(column):
+def _get_field_name(cell):
     field_name = None
-    if 'field' in column:
-        field_name = column['field'].name
+    if 'field' in cell:
+        field_name = cell['field'].name
     return field_name
 
 
@@ -88,11 +88,11 @@ def _slugify(string):
     return string
 
 
-def _swap_fields(column1, column2):
-    field1 = column1.get('field')
-    column1['field'] = column2.get('field')
-    column2['field'] = field1
-    if column1['field'] is None:
-        del column1['field']
-    if column2['field'] is None:
-        del column2['field']
+def _swap_fields(cell1, cell2):
+    field1 = cell1.get('field')
+    cell1['field'] = cell2.get('field')
+    cell2['field'] = field1
+    if cell1['field'] is None:
+        del cell1['field']
+    if cell2['field'] is None:
+        del cell2['field']
