c863c16dce3cf6e568d6115749d6758ac792ddac
roll <eskarev@gmail.com>
2017-08-22 11:24:32 +0300

    Added Python 3.6 to tests

--- tests/checks/test_minimum_constraint.py
+++ tests/checks/test_minimum_constraint.py
@@ -19,7 +19,7 @@ def test_check_minimum_constraint(log):
         [6],
     ]
     schema = {'fields': [
-        {'name': 'row', 'type': 'string'},
+        {'name': 'row', 'type': 'integer'},
         {'name': 'score', 'type': 'integer', 'constraints': {'minimum': 2}}
     ]}
     report = validate(source, schema=schema, checks=[
