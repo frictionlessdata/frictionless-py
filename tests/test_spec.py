c014ebc33b5d56e2ba2877327d75433bd4981f3b
roll <eskarev@gmail.com>
2017-08-22 15:29:41 +0300

    Run spec up-to-date only on CI

--- tests/test_spec.py
+++ tests/test_spec.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import io
+import os
 import json
 import pytest
 import requests
@@ -13,6 +14,7 @@ from goodtables.spec import spec
 
 # Tests
 
+@pytest.mark.skipif(os.environ.get('TRAVIS_BRANCH') != 'master', reason='CI')
 def test_spec_is_up_to_date():
     origin_spec = requests.get('https://raw.githubusercontent.com/frictionlessdata/data-quality-spec/master/spec.json').json()
     assert spec == origin_spec, 'run `make spec` to update the spec'
