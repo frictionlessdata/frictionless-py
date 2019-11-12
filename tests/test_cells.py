14077b077bf81138a6c127b59bda4458d0d8cd77
Joe Staines <joe.staines@nanoporetech.com>
2019-11-12 19:12:23 +0000

    add new tests for test_cells.py

--- tests/test_cells.py
+++ tests/test_cells.py
@@ -1,6 +1,17 @@
 import pytest
 import goodtables.cells
 
+class MockField(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        if isinstance(other, MockField):
+            return self.name == other.name
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 class TestCells(object):
     def test_create_header_cells(self):
@@ -92,6 +103,60 @@ class TestCells(object):
         ]
 
         assert cells == [dict(cell) for cell in expected_cells]
+
+    def test_create_cell_for_optional_field_that_is_present(self):
+        headers = [
+            'header1',
+            'header2'
+        ]
+        schema_fields = [
+            MockField('header1'),
+            MockField('header2')
+        ]
+        optional_fields = ['header2']
+
+        cells = goodtables.cells.create_cells(headers, schema_fields, optional_fields)
+        expected_cells = [
+            goodtables.cells.create_cell('header1', 'header1', MockField('header1'), 1),
+            goodtables.cells.create_cell('header2', 'header2', MockField('header2'), 2)
+        ]
+
+        assert cells == expected_cells
+
+    def test_cell_not_created_for_optional_value_that_is_not_present(self):
+        headers = [
+            'header1',
+        ]
+        schema_fields = [
+            MockField('header1'),
+            MockField('header2')
+        ]
+        optional_fields = ['header2']
+
+        cells = goodtables.cells.create_cells(headers, schema_fields, optional_fields)
+        expected_cells = [
+            goodtables.cells.create_cell('header1', 'header1', MockField('header1'), 1),
+        ]
+
+        assert cells == expected_cells
+        
+    def test_cell_created_when_field_in_schema_but_is_not_optional(self):
+        headers = [
+            'header1',
+        ]
+        schema_fields = [
+            MockField('header1'),
+            MockField('header2')
+        ]
+        optional_fields = []
+
+        cells = goodtables.cells.create_cells(headers, schema_fields, optional_fields)
+        expected_cells = [
+            goodtables.cells.create_cell('header1', 'header1', MockField('header1'), 1),
+            goodtables.cells.create_cell(None, None, MockField('header2'), 2)
+        ]
+
+        assert cells == expected_cells
 
     def test_dict_has_expected_keys(self, cell):
         expected_keys = [
