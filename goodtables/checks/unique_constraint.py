803adcd39f0d6644bcaf69979c893c8948d39ef4
didiez <diegodiez.ddr@gmail.com>
2020-03-18 21:55:50 +0100

    This fixes https://github.com/frictionlessdata/goodtables-py/issues/342 but feels like a hack. I'm sure there is a better and elegant way to solve this.

--- goodtables/checks/unique_constraint.py
+++ goodtables/checks/unique_constraint.py
@@ -35,36 +35,37 @@ class UniqueConstraint(object):
                 for _, cell in enumerate(cells, start=1)
                 if cell["column-number"] in column_numbers
             )
-            column_values = tuple(cell.get('value') for cell in column_cells)
-            row_number = column_cells[0]['row-number']
+            if column_cells:
+                column_values = tuple(cell.get('value') for cell in column_cells)
+                row_number = column_cells[0]['row-number']
 
-            all_values_are_none = (set(column_values) == {None})
-            if not all_values_are_none:
-                if len(cache[column_values]) > 0:
-                    if len(cache[column_values]) <= 5:
-                        message_substitutions = {
-                            "row_numbers": ", ".join(
-                                map(str, cache[column_values] + [row_number])
-                            ),
-                        }
-                    else:
-                        message_substitutions = {
-                            "row_numbers": "{rows} and {count} others".format(
-                                rows=", ".join(map(str, cache[column_values][:3] + [row_number])),
-                                count=len(cache[column_values]) - 4
-                            )
-                        }
+                all_values_are_none = (set(column_values) == {None})
+                if not all_values_are_none:
+                    if len(cache[column_values]) > 0:
+                        if len(cache[column_values]) <= 5:
+                            message_substitutions = {
+                                "row_numbers": ", ".join(
+                                    map(str, cache[column_values] + [row_number])
+                                ),
+                            }
+                        else:
+                            message_substitutions = {
+                                "row_numbers": "{rows} and {count} others".format(
+                                    rows=", ".join(map(str, cache[column_values][:3] + [row_number])),
+                                    count=len(cache[column_values]) - 4
+                                )
+                            }
 
-                    # FIXME: The unique constraint can be related to multiple
-                    # columns (e.g. a composite primary key), but here we only
-                    # pass the 1st column.
-                    error = Error(
-                        'unique-constraint',
-                        column_cells[0],
-                        message_substitutions=message_substitutions
-                    )
-                    errors.append(error)
-                cache[column_values].append(row_number)
+                        # FIXME: The unique constraint can be related to multiple
+                        # columns (e.g. a composite primary key), but here we only
+                        # pass the 1st column.
+                        error = Error(
+                            'unique-constraint',
+                            column_cells[0],
+                            message_substitutions=message_substitutions
+                        )
+                        errors.append(error)
+                    cache[column_values].append(row_number)
 
         return errors
 
