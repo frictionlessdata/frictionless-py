ed9d37e60b7ff91f67b4284f0b3d83aca4c2aa8f
roll <roll@users.noreply.github.com>
2020-04-27 14:03:29 +0300

    Rebase on tableschema@1.16.4 to fix non-finite decimals handling (#363)
    * Added a datapackage
    
    * Added a failing test
    
    * Upgrade to tableschema@1.16.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ INSTALL_REQUIRES = [
     'simpleeval>=0.9',
     'statistics>=1.0',
     'tabulator>=1.29',
-    'tableschema>=1.16.1',
+    'tableschema>=1.16.4',
     'datapackage>=1.10',
 ]
 INSTALL_FORMAT_ODS_REQUIRES = [
