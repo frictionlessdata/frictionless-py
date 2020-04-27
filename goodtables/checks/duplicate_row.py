d88d150d4c8cc753a3591ac26d464c9b503a95bf
roll <eskarev@gmail.com>
2020-04-27 17:42:54 +0300

    Fixed the algorithm

--- goodtables/checks/duplicate_row.py
+++ goodtables/checks/duplicate_row.py
@@ -26,7 +26,7 @@ class DuplicateRow(object):
         try:
             values = list(cell.get('value') for cell in cells)
             # https://github.com/frictionlessdata/goodtables-py/issues/329
-            pointer = hash(json.dumps(values + [len(values)]))
+            pointer = hash(json.dumps(list(str(len(value)) + value for value in values)))
             references = self.__row_index.setdefault(pointer, [])
         except TypeError:
             pointer = None
