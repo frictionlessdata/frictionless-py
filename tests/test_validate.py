26c9524af223ecd9fdf456dd5ae5edc487695d00
roll <roll@users.noreply.github.com>
2020-04-27 13:02:28 +0300

    Added `order_fields` test related to skipping checks (#361)
    * Added a failing test
    
    * Fixed the test

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -444,3 +444,18 @@ def test_validate_invalid_table_schema_issue_304(log):
     ]}
     report = validate(source, schema=schema)
     assert not report['valid']
+
+
+def test_validate_order_fields_issue_313(log):
+    source = 'data/order_fields_313.xlsx'
+    schema = {'fields': [
+        { 'name': 'Column_1', 'type': 'string', },
+        { 'name': 'Column_2', 'type': 'string', 'constraints': { 'required': True } },
+        { 'name': 'Column_3', 'type': 'string' },
+        { 'name': 'Column_4', 'type': 'string' },
+        { 'name': 'Column_5', 'type': 'string' }
+    ]}
+    # For now, the "non-matching-header" check is required to order the fields
+    checks = ['non-matching-header', 'required-constraint']
+    report = validate(source, schema=schema, checks=checks, order_fields=True)
+    assert report['valid']
