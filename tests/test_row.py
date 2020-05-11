322a6cf4632cdc0796ef271579abd70362aed0bd
roll <eskarev@gmail.com>
2020-05-11 09:15:29 +0300

    Fixed tests

--- tests/test_row.py
+++ tests/test_row.py
@@ -4,9 +4,9 @@ from goodtables import Row
 # Helpers
 
 
-def create_row(cells, *, field_names=[], line_number=1, row_number=1):
+def create_row(cells, *, field_names=[], row_position=1, row_number=1):
     return Row(
-        cells, field_names=field_names, line_number=line_number, row_number=row_number
+        cells, field_names=field_names, row_position=row_position, row_number=row_number
     )
 
 
@@ -16,7 +16,7 @@ def create_row(cells, *, field_names=[], line_number=1, row_number=1):
 def test_basic():
     row = create_row([1, 2, 3])
     assert row.cells == [1, 2, 3]
-    assert row.line_number == 1
+    assert row.row_position == 1
     assert row.row_number == 1
     assert row.field_names == ['field1', 'field2', 'field3']
     assert row.deletions == {}
