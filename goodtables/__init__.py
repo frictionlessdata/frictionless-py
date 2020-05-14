c24defec332a676eba77f8efb40d373e78f6bcd5
roll <eskarev@gmail.com>
2020-05-14 12:07:16 +0300

    Renamed validate_dataset->task

--- goodtables/__init__.py
+++ goodtables/__init__.py
@@ -2,7 +2,7 @@ from .dialects import validate_csv, validate_excel, validate_json
 from .headers import Headers
 from .report import Report, ReportTable
 from .row import Row
-from .sources import validate_dataset, validate_package, validate_resource, validate_table
+from .sources import validate_package, validate_resource, validate_table, validate_task
 from .validate import validate
 from . import errors
 from . import exceptions
