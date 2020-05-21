bb6a1486b771dead82327cb6880948c23b6c4d67
roll <eskarev@gmail.com>
2020-05-21 16:28:20 +0300

    Migrated dataset tests

--- tests/sources/test_dataset.py
+++ tests/sources/test_dataset.py
@@ -28,6 +28,29 @@ def test_validate_multiple_invalid():
     ]
 
 
+def test_validate_multiple_invalid_with_schema():
+    report = validate(
+        [
+            {
+                'source': 'data/table.csv',
+                'schema': {'fields': [{'name': 'bad'}, {'name': 'name'}]},
+            },
+            {'source': 'data/invalid.csv'},
+        ],
+    )
+    assert report.flatten(['tableNumber', 'rowPosition', 'fieldPosition', 'code']) == [
+        [1, None, 1, 'non-matching-header'],
+        [2, None, 3, 'blank-header'],
+        [2, None, 4, 'duplicate-header'],
+        [2, 2, 3, 'missing-cell'],
+        [2, 2, 4, 'missing-cell'],
+        [2, 3, 3, 'missing-cell'],
+        [2, 3, 4, 'missing-cell'],
+        [2, 4, None, 'blank-row'],
+        [2, 5, 5, 'extra-cell'],
+    ]
+
+
 def test_validate_with_datapackage():
     report = validate([{'source': 'data/valid/datapackage.json'}])
     assert report['valid']
