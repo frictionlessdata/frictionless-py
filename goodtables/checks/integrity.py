70f34d295328abf3e7169e56adbb82b10be0b15c
roll <eskarev@gmail.com>
2020-06-02 09:21:51 +0300

    Added metadata profile to IntegrityCheck

--- goodtables/checks/integrity.py
+++ goodtables/checks/integrity.py
@@ -3,20 +3,27 @@ from ..check import Check
 from ..errors import SizeError, HashError
 
 
-# TODO: add metadata profile to validate descriptor
 class IntegrityCheck(Check):
+    metadata_profile = {  # type: ignore
+        'type': 'object',
+        'properties': {
+            'size': {'type': ['number', 'null']},
+            'hash': {'type': ['string', 'null']},
+        },
+    }
+
     def validate_finish(self):
         errors = []
 
         # Size error
-        if self['size']:
+        if self.get('size'):
             if self['size'] != self.stream.size:
                 details = 'expected is "%s" and actual is "%s"'
                 details = details % (self['size'], self.stream.size)
                 errors.append(SizeError(details=details))
 
         # Hash error
-        if self['hash']:
+        if self.get('hash'):
             hashing_digest = helpers.parse_hashing_digest(self['hash'])
             if hashing_digest != self.stream.hash:
                 details = 'expected is "%s" and actual is "%s"'
