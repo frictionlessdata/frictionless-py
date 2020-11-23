59e084c5c014d65fc4ef3d10741b77d7d9aba28a
roll <eskarev@gmail.com>
2020-11-23 10:16:36 +0100

    Fixed resource with filelike

--- tests/loaders/test_stream.py
+++ tests/loaders/test_stream.py
@@ -1,4 +1,3 @@
-import io
 from frictionless import Table, Resource
 
 
@@ -6,14 +5,16 @@ from frictionless import Table, Resource
 
 
 def test_table_stream():
-    source = io.open("data/table.csv", mode="rb")
-    with Table(source, format="csv") as table:
-        assert table.header == ["id", "name"]
-        assert table.read_data() == [["1", "english"], ["2", "中国人"]]
+    with open("data/table.csv", mode="rb") as file:
+        with Table(file, format="csv") as table:
+            assert table.header == ["id", "name"]
+            assert table.read_data() == [["1", "english"], ["2", "中国人"]]
 
 
 def test_resource_stream():
-    source = io.open("data/table.csv", mode="rb")
-    resource = Resource(path=source, format="csv")
-    assert resource.read_header() == ["id", "name"]
-    assert resource.read_data() == [["1", "english"], ["2", "中国人"]]
+    with open("data/table.csv", mode="rb") as file:
+        resource = Resource(path=file, format="csv")
+        assert resource.read_rows() == [
+            {"id": 1, "name": "english"},
+            {"id": 2, "name": "中国人"},
+        ]
