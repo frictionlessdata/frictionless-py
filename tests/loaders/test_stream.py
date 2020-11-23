94b277a270cd37aa160a6e3e455426b7339b117c
roll <eskarev@gmail.com>
2020-11-23 10:02:10 +0100

    Added a failing test

--- tests/loaders/test_stream.py
+++ tests/loaders/test_stream.py
@@ -1,5 +1,5 @@
 import io
-from frictionless import Table
+from frictionless import Table, Resource
 
 
 # Read
@@ -10,3 +10,10 @@ def test_table_stream():
     with Table(source, format="csv") as table:
         assert table.header == ["id", "name"]
         assert table.read_data() == [["1", "english"], ["2", "中国人"]]
+
+
+def test_resource_stream():
+    source = io.open("data/table.csv", mode="rb")
+    resource = Resource(path=source, format="csv")
+    assert resource.read_header() == ["id", "name"]
+    assert resource.read_data() == [["1", "english"], ["2", "中国人"]]
