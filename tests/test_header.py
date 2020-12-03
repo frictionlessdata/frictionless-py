495a7d6cff2c2d1ca9f01c7f9ff9f0f46da7e2ba
roll <roll@users.noreply.github.com>
2020-12-03 15:34:47 +0100

    Fixed Header not being an original one (#572)

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,4 +1,4 @@
-from frictionless import Header, Field, Schema
+from frictionless import Header, Field, Schema, Resource
 
 
 # General
@@ -9,6 +9,22 @@ def test_basic():
     assert header.field_positions == [1, 2, 3]
     assert header.errors == []
     assert header == ["field1", "field2", "field3"]
+
+
+def test_extra_header():
+    schema = Schema(fields=[Field(name="id")])
+    resource = Resource(path="data/table.csv", schema=schema)
+    header = resource.read_header()
+    assert header == ["id", "name"]
+    assert header.valid is False
+
+
+def test_missing_header():
+    schema = Schema(fields=[Field(name="id"), Field(name="name"), Field(name="extra")])
+    resource = Resource(path="data/table.csv", schema=schema)
+    header = resource.read_header()
+    assert header == ["id", "name"]
+    assert header.valid is False
 
 
 # Helpers
