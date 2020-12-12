b42cd75eecd49b375672124a5813a98961f02d88
roll <eskarev@gmail.com>
2020-12-12 15:21:16 +0300

    Migrated gsheets

--- tests/plugins/test_gsheets.py
+++ tests/plugins/test_gsheets.py
@@ -16,24 +16,24 @@ pytestmark = pytest.mark.skipif(
 # Parser
 
 
-@pytest.mark.ci
-def test_table_gsheets():
+@pytest.mark.vcr
+def test_gsheets_parser():
     source = "https://docs.google.com/spreadsheets/d/1mHIWnDvW9cALRMq9OdNfRwjAthCUFUOACPp0Lkyl7b4/edit?usp=sharing"
     with Table(source) as table:
         assert table.header == ["id", "name"]
         assert table.read_data() == [["1", "english"], ["2", "中国人"]]
 
 
-@pytest.mark.ci
-def test_table_gsheets_with_gid():
+@pytest.mark.vcr
+def test_gsheets_parser_with_gid():
     source = "https://docs.google.com/spreadsheets/d/1mHIWnDvW9cALRMq9OdNfRwjAthCUFUOACPp0Lkyl7b4/edit#gid=960698813"
     with Table(source) as table:
         assert table.header == ["id", "name"]
         assert table.read_data() == [["2", "中国人"], ["3", "german"]]
 
 
-@pytest.mark.ci
-def test_table_gsheets_bad_url():
+@pytest.mark.vcr
+def test_gsheets_parser_bad_url():
     table = Table("https://docs.google.com/spreadsheets/d/bad")
     with pytest.raises(FrictionlessException) as excinfo:
         table.open()
@@ -44,7 +44,7 @@ def test_table_gsheets_bad_url():
 
 @pytest.mark.ci
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="Speed up CI")
-def test_table_gsheets_write():
+def test_gsheets_parser_write():
     path = "https://docs.google.com/spreadsheets/d/1F2OiYmaf8e3x7jSc95_uNgfUyBlSXrcRg-4K_MFNZQI/edit"
 
     # Write
