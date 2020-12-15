a74c1d10034faa0a25d347e8863bfc58a6b0a2e0
roll <eskarev@gmail.com>
2020-12-15 19:53:40 +0300

    Added Row3 draft

--- frictionless/row3.py
+++ frictionless/row3.py
@@ -7,7 +7,7 @@ from . import helpers
 from . import errors
 
 
-class Row2(dict):
+class Row3(list):
     """Row representation
 
     API      | Usage
@@ -41,7 +41,7 @@ class Row2(dict):
         row_position,
         row_number,
     ):
-        self.__cells = cells
+        super().__init__(cells)
         self.__schema = schema
         self.__field_map = field_map
         self.__field_positions = field_positions
@@ -49,110 +49,17 @@ class Row2(dict):
         self.__row_number = row_number
         self.__blank_cells = {}
         self.__error_cells = {}
-        self.__partial = True
         self.__errors = []
-
-    def __missing__(self, key):
-        if self.__partial:
-            value = self.__process(key)
-            self[key] = value
-            return value
-        raise KeyError(key)
-
-    def __str__(self):
-        if self.__partial:
-            self.__process()
-        return super().__str__()
-
-    def __repr__(self):
-        if self.__partial:
-            self.__process()
-        return super().__repr__()
-
-    def __iter__(self):
-        if self.__partial:
-            self.__process()
-        return super().__iter__()
-
-    def __len__(self):
-        if self.__partial:
-            self.__process()
-        return super().__len__()
-
-    def __contains__(self, key):
-        if self.__partial:
-            self.__process()
-        return super().__contains__(key)
-
-    def __reversed__(self, key):
-        if self.__partial:
-            self.__process()
-        return super().__reversed__(key)
-
-    def keys(self):
-        if self.__partial:
-            self.__process()
-        return super().keys()
-
-    def values(self):
-        if self.__partial:
-            self.__process()
-        return super().values()
-
-    def items(self):
-        if self.__partial:
-            self.__process()
-        return super().items()
+        self.__processed = False
+        self.__data = RowData(self, field_map=field_map)
 
     @cached_property
-    def blank_cells(self):
-        """A mapping indexed by a field name with blank cells before parsing
-
-        Returns:
-            dict: row blank cells
-        """
-        if self.__partial:
-            self.__process()
-        return self.__blank_cells
-
-    @cached_property
-    def error_cells(self):
-        """A mapping indexed by a field name with error cells before parsing
-
-        Returns:
-            dict: row error cells
-        """
-        if self.__partial:
-            self.__process()
-        return self.__error_cells
-
-    @cached_property
-    def errors(self):
+    def data(self):
         """
         Returns:
-            Error[]: row errors
+            dict: row data
         """
-        if self.__partial:
-            self.__process()
-        return self.__errors
-
-    @cached_property
-    def valid(self):
-        """
-        Returns:
-            bool: if row valid
-        """
-        if self.__partial:
-            self.__process()
-        return not self.__errors
-
-    @cached_property
-    def cells(self):
-        """
-        Returns:
-            any[]: original row cells
-        """
-        return self.__cells
+        return self.__data
 
     @cached_property
     def schema(self):
@@ -185,6 +92,161 @@ class Row2(dict):
             int: row number from 1
         """
         return self.__row_number
+
+    @cached_property
+    def blank_cells(self):
+        """A mapping indexed by a field name with blank cells before parsing
+
+        Returns:
+            dict: row blank cells
+        """
+        self.process()
+        return self.__blank_cells
+
+    @cached_property
+    def error_cells(self):
+        """A mapping indexed by a field name with error cells before parsing
+
+        Returns:
+            dict: row error cells
+        """
+        self.process()
+        return self.__error_cells
+
+    @cached_property
+    def errors(self):
+        """
+        Returns:
+            Error[]: row errors
+        """
+        self.process()
+        return self.__errors
+
+    @cached_property
+    def valid(self):
+        """
+        Returns:
+            bool: if row valid
+        """
+        self.process()
+        return not self.__errors
+
+    # Process
+
+    def process(self, key=None):
+
+        # Exit if processed
+        if self.__processed:
+            if key:
+                raise KeyError(key)
+            return
+
+        # Iterate cells
+        cells = self.__cells
+        fields = self.__schema.fields
+        field_positions = self.__field_positions
+        for field_name in [key] if key else self.__schema.field_names:
+            # TODO: improve key error
+            field, field_position, field_number = self.__field_map[field_name]
+
+            # Read cell
+            # TODO: recover
+            #  source = None
+            #  if len(self.__cells) >= field_number:
+            source = self.__cells[field_number - 1]
+            target, notes = field.read_cell(source)
+            type_note = notes.pop("type", None) if notes else None
+            if target is None and not type_note:
+                self.__blank_cells[field.name] = source
+
+            # Type error
+            if type_note:
+                self.__error_cells[field.name] = source
+                self.__errors.append(
+                    errors.TypeError(
+                        note=type_note,
+                        cells=list(map(str, self.__cells)),
+                        row_number=self.__row_number,
+                        row_position=self.__row_position,
+                        cell=str(source),
+                        field_name=field.name,
+                        field_number=field_number,
+                        field_position=field_position,
+                    )
+                )
+
+            # Constraint errors
+            if notes:
+                for note in notes.values():
+                    self.__errors.append(
+                        errors.ConstraintError(
+                            note=note,
+                            cells=list(map(str, self.__cells)),
+                            row_number=self.__row_number,
+                            row_position=self.__row_position,
+                            cell=str(source),
+                            field_name=field.name,
+                            field_number=field_number,
+                            field_position=field_position,
+                        )
+                    )
+
+            # Set/return value
+            dict.__setitem__(self.__data, field.name, target)
+            if key:
+                return target
+
+        # Extra cells
+        if len(fields) < len(cells):
+            iterator = cells[len(fields) :]
+            start = max(field_positions[: len(fields)]) + 1
+            for field_position, cell in enumerate(iterator, start=start):
+                self.__errors.append(
+                    errors.ExtraCellError(
+                        note="",
+                        cells=list(map(str, cells)),
+                        row_number=self.__row_number,
+                        row_position=self.__row_position,
+                        cell=str(cell),
+                        field_name="",
+                        field_number=len(fields) + field_position - start,
+                        field_position=field_position,
+                    )
+                )
+
+        # Missing cells
+        if len(fields) > len(cells):
+            start = len(cells) + 1
+            iterator = zip_longest(field_positions[len(cells) :], fields[len(cells) :])
+            for field_number, (field_position, field) in enumerate(iterator, start=start):
+                if field is not None:
+                    self.__errors.append(
+                        errors.MissingCellError(
+                            note="",
+                            cells=list(map(str, cells)),
+                            row_number=self.__row_number,
+                            row_position=self.__row_position,
+                            cell="",
+                            field_name=field.name,
+                            field_number=field_number,
+                            field_position=field_position
+                            or max(field_positions) + field_number - start + 1,
+                        )
+                    )
+
+        # Blank row
+        if len(fields) == len(self.__blank_cells):
+            self.__errors = [
+                errors.BlankRowError(
+                    note="",
+                    cells=list(map(str, cells)),
+                    row_number=self.__row_number,
+                    row_position=self.__row_position,
+                )
+            ]
+
+        # Set processed
+        self.__processed = True
 
     # Import/Export
 
@@ -242,113 +304,49 @@ class Row2(dict):
             return result
         return list(self.values())
 
-    # Process
 
-    def __process(self, key=None):
-        #  print('validate')
-        cells = self.__cells
-        fields = self.__schema.fields
-        field_positions = self.__field_positions
+class RowData(dict):
+    def __init(self, row, *, field_map):
+        self.__row = row
+        self.__field_map = field_map
 
-        # Type/Constraint error
-        for field_name in [key] if key else self.__schema.field_names:
-            field, field_position, field_number = self.__field_map[field_name]
+    def __missing__(self, key):
+        return self.__row.process(key)
 
-            # Read cell
-            # TODO: recover
-            #  source = None
-            #  if len(self.__cells) >= field_number:
-            source = self.__cells[field_number - 1]
-            target, notes = field.read_cell(source)
-            type_note = notes.pop("type", None) if notes else None
-            if target is None and not type_note:
-                self.__blank_cells[field.name] = source
+    def __str__(self):
+        self.__row.process()
+        return super().__str__()
 
-            # Type error
-            if type_note:
-                self.__error_cells[field.name] = source
-                self.__errors.append(
-                    errors.TypeError(
-                        note=type_note,
-                        cells=list(map(str, self.__cells)),
-                        row_number=self.__row_number,
-                        row_position=self.__row_position,
-                        cell=str(source),
-                        field_name=field.name,
-                        field_number=field_number,
-                        field_position=field_position,
-                    )
-                )
+    def __repr__(self):
+        self.__row.process()
+        return super().__repr__()
 
-            # Constraint errors
-            if notes:
-                for note in notes.values():
-                    self.__errors.append(
-                        errors.ConstraintError(
-                            note=note,
-                            cells=list(map(str, self.__cells)),
-                            row_number=self.__row_number,
-                            row_position=self.__row_position,
-                            cell=str(source),
-                            field_name=field.name,
-                            field_number=field_number,
-                            field_position=field_position,
-                        )
-                    )
+    def __iter__(self):
+        return iter(self.__row.schema.field_names)
 
-            # Set/return value
-            self[field.name] = target
-            if key:
-                return target
+    def __len__(self):
+        return len(self.__row.schema.fields)
 
-        # Extra cells
-        if len(fields) < len(cells):
-            iterator = cells[len(fields) :]
-            start = max(field_positions[: len(fields)]) + 1
-            for field_position, cell in enumerate(iterator, start=start):
-                self.__errors.append(
-                    errors.ExtraCellError(
-                        note="",
-                        cells=list(map(str, cells)),
-                        row_number=self.__row_number,
-                        row_position=self.__row_position,
-                        cell=str(cell),
-                        field_name="",
-                        field_number=len(fields) + field_position - start,
-                        field_position=field_position,
-                    )
-                )
+    def __contains__(self, key):
+        return key in self.__field_map
 
-        # Missing cells
-        if len(fields) > len(cells):
-            start = len(cells) + 1
-            iterator = zip_longest(field_positions[len(cells) :], fields[len(cells) :])
-            for field_number, (field_position, field) in enumerate(iterator, start=start):
-                if field is not None:
-                    self.__errors.append(
-                        errors.MissingCellError(
-                            note="",
-                            cells=list(map(str, cells)),
-                            row_number=self.__row_number,
-                            row_position=self.__row_position,
-                            cell="",
-                            field_name=field.name,
-                            field_number=field_number,
-                            field_position=field_position
-                            or max(field_positions) + field_number - start + 1,
-                        )
-                    )
+    def __reversed__(self, key):
+        return reversed(self)
 
-        # Blank row
-        if len(fields) == len(self.__blank_cells):
-            self.__errors = [
-                errors.BlankRowError(
-                    note="",
-                    cells=list(map(str, cells)),
-                    row_number=self.__row_number,
-                    row_position=self.__row_position,
-                )
-            ]
+    def __setitem__(self, key, value):
+        # TODO: improve key error
+        field, field_position, field_number = self.__field_map[key]
+        self.__row[field_number] = value
+        dict.__setitem__(self, key, self.__row.process(key))
 
-        # Set partial flag
-        self.__partial = False
+    def keys(self):
+        self.__row.process()
+        return super().keys()
+
+    def values(self):
+        self.__row.process()
+        return super().values()
+
+    def items(self):
+        self.__row.process()
+        return super().items()
