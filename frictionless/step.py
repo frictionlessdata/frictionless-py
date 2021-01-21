6f72bbc695077243a6e40b46fca8ea7764b4f80e
roll <eskarev@gmail.com>
2021-01-21 11:43:33 +0300

    Rebased on system.create_step

--- frictionless/step.py
+++ frictionless/step.py
@@ -1,9 +1,16 @@
 from .metadata import Metadata
 
 
+# TODO: finalize
 class Step(Metadata):
     code = "step"
 
     def __init__(self, descriptor=None):
         super().__init__(descriptor)
         self.setinitial("code", self.code)
+
+    def transorm_resource(self, source, target):
+        pass
+
+    def transorm_package(self, source, target):
+        pass
