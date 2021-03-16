f92a697a47b07c7f61f14d186ad2394e86fe55ee
roll <eskarev@gmail.com>
2021-03-16 17:36:07 +0300

    Fixed test_resource_hashing

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -457,20 +457,20 @@ def test_resource_format_error_non_matching_format():
 # Hashing
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_hashing():
     with Resource("data/table.csv") as resource:
         resource.read_rows()
         assert resource.hashing == "md5"
-        assert resource.stats["hash"] == "6c2c61dd9b0e9c6876139a449ed87933"
+        if IS_UNIX:
+            assert resource.stats["hash"] == "6c2c61dd9b0e9c6876139a449ed87933"
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_hashing_provided():
     with Resource("data/table.csv", hashing="sha1") as resource:
         resource.read_rows()
         assert resource.hashing == "sha1"
-        assert resource.stats["hash"] == "db6ea2f8ff72a9e13e1d70c28ed1c6b42af3bb0e"
+        if IS_UNIX:
+            assert resource.stats["hash"] == "db6ea2f8ff72a9e13e1d70c28ed1c6b42af3bb0e"
 
 
 def test_resource_hashing_error_bad_hashing():
