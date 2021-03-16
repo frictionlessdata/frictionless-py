c0841fc81a38321f618e088ea450eba943a7d095
roll <eskarev@gmail.com>
2021-03-16 18:05:37 +0300

    Enabled more tests

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -541,7 +541,8 @@ def test_resource_encoding_error_non_matching_encoding():
         resource.open()
     error = excinfo.value.error
     assert error.code == "encoding-error"
-    assert error.note[:51] == "'ascii' codec can't decode byte 0xe4 in position 20"
+    if IS_UNIX:
+        assert error.note[:51] == "'ascii' codec can't decode byte 0xe4 in position 20"
 
 
 # Innerpath
@@ -1460,7 +1461,6 @@ def test_resource_schema_source_data():
 
 
 @pytest.mark.vcr
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_schema_source_remote():
     descriptor = {
         "name": "name",
@@ -1501,7 +1501,6 @@ def test_resource_schema_from_path_with_basepath():
 
 
 @pytest.mark.vcr
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_schema_from_path_remote():
     resource = Resource(BASEURL % "data/resource-with-dereferencing.json")
     assert resource == {
@@ -1640,67 +1639,67 @@ def test_resource_schema_foreign_keys_invalid():
 # Stats
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_stats_hash():
     with Resource("data/doublequote.csv") as resource:
         resource.read_rows()
         assert resource.hashing == "md5"
-        assert resource.stats["hash"] == "d82306001266c4343a2af4830321ead8"
+        if IS_UNIX:
+            assert resource.stats["hash"] == "d82306001266c4343a2af4830321ead8"
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_stats_hash_md5():
     with Resource("data/doublequote.csv", hashing="md5") as resource:
         resource.read_rows()
         assert resource.hashing == "md5"
-        assert resource.stats["hash"] == "d82306001266c4343a2af4830321ead8"
+        if IS_UNIX:
+            assert resource.stats["hash"] == "d82306001266c4343a2af4830321ead8"
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_stats_hash_sha1():
     with Resource("data/doublequote.csv", hashing="sha1") as resource:
         resource.read_rows()
         assert resource.hashing == "sha1"
-        assert resource.stats["hash"] == "2842768834a6804d8644dd689da61c7ab71cbb33"
+        if IS_UNIX:
+            assert resource.stats["hash"] == "2842768834a6804d8644dd689da61c7ab71cbb33"
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_stats_hash_sha256():
     with Resource("data/doublequote.csv", hashing="sha256") as resource:
         resource.read_rows()
         assert resource.hashing == "sha256"
-        assert (
-            resource.stats["hash"]
-            == "41fdde1d8dbcb3b2d4a1410acd7ad842781f076076a73b049863d6c1c73868db"
-        )
+        if IS_UNIX:
+            assert (
+                resource.stats["hash"]
+                == "41fdde1d8dbcb3b2d4a1410acd7ad842781f076076a73b049863d6c1c73868db"
+            )
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_stats_hash_sha512():
     with Resource("data/doublequote.csv", hashing="sha512") as resource:
         resource.read_rows()
         assert resource.hashing == "sha512"
-        assert (
-            resource.stats["hash"]
-            == "fa555b28a01959c8b03996cd4757542be86293fd49641d61808e4bf9fe4115619754aae9ae6af6a0695585eaade4488ce00dfc40fc4394b6376cd20d6967769c"
-        )
+        if IS_UNIX:
+            assert (
+                resource.stats["hash"]
+                == "fa555b28a01959c8b03996cd4757542be86293fd49641d61808e4bf9fe4115619754aae9ae6af6a0695585eaade4488ce00dfc40fc4394b6376cd20d6967769c"
+            )
 
 
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_stats_hash_compressed():
     with Resource("data/doublequote.csv.zip") as resource:
         resource.read_rows()
         assert resource.hashing == "md5"
-        assert resource.stats["hash"] == "2a72c90bd48c1fa48aec632db23ce8f7"
+        if IS_UNIX:
+            assert resource.stats["hash"] == "2a72c90bd48c1fa48aec632db23ce8f7"
 
 
 @pytest.mark.vcr
-@pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
 def test_resource_stats_hash_remote():
     with Resource(BASEURL % "data/doublequote.csv") as resource:
         resource.read_rows()
         assert resource.hashing == "md5"
-        assert resource.stats["hash"] == "d82306001266c4343a2af4830321ead8"
+        if IS_UNIX:
+            assert resource.stats["hash"] == "d82306001266c4343a2af4830321ead8"
 
 
 @pytest.mark.skipif(helpers.is_platform("windows"), reason="It doesn't work for Windows")
