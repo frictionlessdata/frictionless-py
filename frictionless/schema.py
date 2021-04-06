6bde31342dd7b1ce75216b44f59d54f8c06fe4a4
roll <eskarev@gmail.com>
2021-04-06 10:37:42 +0300

    Improved Schema.from_jsonschema

--- frictionless/schema.py
+++ frictionless/schema.py
@@ -243,8 +243,30 @@ class Schema(Metadata):
         properties = profile.get("properties", {})
         assert isinstance(properties, dict)
         for name, prop in properties.items():
+
+            # Field
+            assert isinstance(name, str)
             assert isinstance(prop, dict)
-            schema.add_field(Field(name=name))
+            field = Field(name=name)
+            schema.add_field(field)
+
+            # Type
+            type = prop.get("type")
+            if type:
+                assert isinstance(type, str)
+                if type in ["string", "integer", "number", "boolean", "object", "array"]:
+                    field.type = type
+
+            # Description
+            description = prop.get("description")
+            if description:
+                assert isinstance(description, str)
+                field.description = description
+
+            # Required
+            if name in required:
+                field.constraints["required"] = True
+
         return schema
 
     # Metadata
