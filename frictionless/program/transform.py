17a781cb4406f13eec9050b7ae94d1af02c5d912
roll <eskarev@gmail.com>
2021-04-22 17:27:51 +0200

    Fixed program.transform swallows errors

--- frictionless/program/transform.py
+++ frictionless/program/transform.py
@@ -1,5 +1,6 @@
 import sys
 import typer
+from ..exception import FrictionlessException
 from ..transform import transform
 from .main import program
 from . import common
@@ -24,7 +25,13 @@ def program_transform(
 
     # Transform source
     try:
-        transform(source)
+        status = transform(source)
+        if not status.valid:
+            # NOTE: improve how we handle/present errors
+            groups = [status.errors] + list(map(lambda task: task.errors, status.tasks))
+            for group in groups:
+                for error in group:
+                    raise FrictionlessException(error)
     except Exception as exception:
         typer.secho(str(exception), err=True, fg=typer.colors.RED, bold=True)
         raise typer.Exit(1)
