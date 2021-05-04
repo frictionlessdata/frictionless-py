f2a85984c0711e63dd2ab0096177f6426e13f0af
roll <eskarev@gmail.com>
2021-05-04 11:16:08 +0200

    Fixed linting

--- tests/validate/test_resource.py
+++ tests/validate/test_resource.py
@@ -1162,5 +1162,5 @@ def test_validate_resource_from_json_format_issue_827():
 
 
 def test_validate_resource_none_is_not_iterable_enum_constraint_issue_833():
-    report = validate("data/issue-833.csv", schema='data/issue-833.json')
+    report = validate("data/issue-833.csv", schema="data/issue-833.json")
     assert report.valid
