ea28cbc072992e30616774a6fb478070b1a59863
roll <roll@users.noreply.github.com>
2021-06-16 11:05:16 +0200

    Add newline characters tests to HtmlParser (#866)
    * Added html newline tests
    
    * Updated tests

--- tests/plugins/test_html.py
+++ tests/plugins/test_html.py
@@ -34,3 +34,23 @@ def test_html_parser_write(tmpdir):
             {"id": 1, "name": "english"},
             {"id": 2, "name": "中国人"},
         ]
+
+
+def test_html_parser_newline_in_cell_issue_865(tmpdir):
+    source = Resource("data/table-with-newline.html")
+    target = source.write(str(tmpdir.join("table.csv")))
+    with target:
+        assert target.header == ["id", "name"]
+        assert target.read_rows() == [
+            {"id": 1, "name": "with newline"},
+            {"id": 2, "name": "with newline"},
+            {"id": 3, "name": "with\nnewline"},
+            {"id": 4, "name": "with\\nnewline"},
+        ]
+
+
+def test_html_parser_newline_in_cell_construction_file_issue_865(tmpdir):
+    source = Resource("data/construction.html")
+    target = source.write(str(tmpdir.join("table.csv")))
+    target.infer(stats=True)
+    assert target.stats["rows"] == 226
