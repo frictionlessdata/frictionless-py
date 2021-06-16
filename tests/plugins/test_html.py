033e2d9c02b5285abb79d8412ff715102291278c
roll <eskarev@gmail.com>
2021-06-16 10:54:15 +0200

    Updated tests

--- tests/plugins/test_html.py
+++ tests/plugins/test_html.py
@@ -43,6 +43,14 @@ def test_html_parser_newline_in_cell_issue_865(tmpdir):
         assert target.header == ["id", "name"]
         assert target.read_rows() == [
             {"id": 1, "name": "with newline"},
-            {"id": 2, "name": "with\\nnewline"},
-            {"id": 3, "name": "with newline"},
+            {"id": 2, "name": "with newline"},
+            {"id": 3, "name": "with\nnewline"},
+            {"id": 4, "name": "with\\nnewline"},
         ]
+
+
+def test_html_parser_newline_in_cell_construction_file_issue_865(tmpdir):
+    source = Resource("data/construction.html")
+    target = source.write(str(tmpdir.join("table.csv")))
+    target.infer(stats=True)
+    assert target.stats["rows"] == 226
