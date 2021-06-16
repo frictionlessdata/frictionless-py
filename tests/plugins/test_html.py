d70fc470ec3ed68b332f5d4867a6f74b40600fae
roll <eskarev@gmail.com>
2021-06-16 10:37:40 +0200

    Added html newline tests

--- tests/plugins/test_html.py
+++ tests/plugins/test_html.py
@@ -34,3 +34,15 @@ def test_html_parser_write(tmpdir):
             {"id": 1, "name": "english"},
             {"id": 2, "name": "中国人"},
         ]
+
+
+def test_html_parser_newline_in_cell_issue_865(tmpdir):
+    source = Resource("data/table-with-newline.html")
+    target = source.write(str(tmpdir.join("table.csv")))
+    with target:
+        assert target.header == ["id", "name"]
+        assert target.read_rows() == [
+            {"id": 1, "name": "with newline"},
+            {"id": 2, "name": "with\\nnewline"},
+            {"id": 3, "name": "with newline"},
+        ]
