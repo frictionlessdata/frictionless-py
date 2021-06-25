0eecf0ad52ab48f7ae25dc12615c5e1434abbd18
roll <roll@users.noreply.github.com>
2021-06-25 17:19:17 +0200

    Improve package/resource metadata validation (#872)
    * Added an invalid data package
    
    * Added a failing test
    
    * Stricten package/resource metadata validation

--- frictionless/package.py
+++ frictionless/package.py
@@ -8,6 +8,7 @@ from .exception import FrictionlessException
 from .metadata import Metadata
 from .detector import Detector
 from .resource import Resource
+from .field import Field
 from .system import system
 from . import helpers
 from . import errors
@@ -688,3 +689,21 @@ class Package(Metadata):
         # Resources
         for resource in self.resources:
             yield from resource.metadata_errors
+
+        # Created
+        if self.get("created"):
+            field = Field(type="datetime")
+            cell = field.read_cell(self.get("created"))[0]
+            if not cell:
+                note = 'property "created" is not valid "datetime"'
+                yield errors.PackageError(note=note)
+
+        # Contributors/Sources
+        for name in ["contributors", "sources"]:
+            for item in self.get(name, []):
+                if item.get("email"):
+                    field = Field(type="string", format="email")
+                    cell = field.read_cell(item.get("email"))[0]
+                    if not cell:
+                        note = f'property "{name}[].email" is not valid "email"'
+                        yield errors.PackageError(note=note)
