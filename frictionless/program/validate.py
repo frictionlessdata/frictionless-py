e16af1bc29966cae42369f512147d752e23f506b
Alex Kreidler <alexkreidler2020@gmail.com>
2021-08-20 10:04:32 -0400

    add for validate

--- frictionless/program/validate.py
+++ frictionless/program/validate.py
@@ -8,6 +8,7 @@ from ..layout import Layout
 from .main import program
 from .. import helpers
 from . import common
+from frictionless import Dialect
 
 
 @program.command(name="validate")
@@ -27,6 +28,10 @@ def program_validate(
     control: str = common.control,
     # Dialect
     dialect: str = common.dialect,
+    sheet: str = common.sheet,
+    table: str = common.table,
+    keys: str = common.keys,
+    keyed: bool = common.keyed,
     # Layout
     header_rows: str = common.header_rows,
     header_join: str = common.header_join,
@@ -89,6 +94,18 @@ def program_validate(
     source = list(source) if len(source) > 1 else (source[0] if source else None)
     control = helpers.parse_json_string(control)
     dialect = helpers.parse_json_string(dialect)
+
+    dialect = Dialect(dialect)
+
+    if sheet:
+        dialect["sheet"] = sheet
+    if table:
+        dialect["table"] = table
+    if keys:
+        dialect["keys"] = keys
+    if keyed:
+        dialect["keyed"] = keyed
+
     header_rows = helpers.parse_csv_string(header_rows, convert=int)
     pick_fields = helpers.parse_csv_string(pick_fields, convert=int, fallback=True)
     skip_fields = helpers.parse_csv_string(skip_fields, convert=int, fallback=True)
