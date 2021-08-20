b7e64518944655cf4636e3b1edd06d3355eae6bc
Your Name <you@example.com>
2021-08-20 09:59:02 -0400

    update to markdown rendering

---
+++ frictionless/to_markdown/README.md
@@ -0,0 +1,3 @@
+# Frictionless to Markdown
+
+Based on Handlebars templates from https://framagit.org/opendataschema/table-schema-to-markdown/.
--- frictionless/to_markdown/render.py
+++ frictionless/to_markdown/render.py
@@ -1,22 +1,62 @@
 # Get a compiler
 from pybars import Compiler
+import json
+import glom
+import os
+from helpers import helpers
+
 compiler = Compiler()
 
+def getText(loc):
+    with open(loc, "r") as file:
+        data = file.read()
+    return data
+
 # Compile the template
-source = u""
+source = getText("./templates/index.hbs")
 template = compiler.compile(source)
+
+lang = "en"
+langLoc = "./i18n/" + lang + ".json"
+print(langLoc)
+intlData = json.loads(getText(langLoc))
 
 
 def intlGet(this, key):
-    pass
+    return glom(intlData, key)
 
-helpers = {'list': _list}
 
-# Add partials
-header = compiler.compile(u'<h1>People</h1>')
-partials = {'header': header}
+# helpers = {'intlGet': intlGet}
+
+partials = {}
+
+
+def loadPartial(location):
+    data = getText(location)
+    p = compiler.compile(data)
+    name = os.path.splitext(location)[0]
+    print(f"loading partial {name}")
+    partials[name] = p
+
+
+ps = [
+    "./templates/partials/fields-as-headings/constraintHeadingProperties.hbs",
+    "./templates/partials/fields-as-table/constraintTableProperties.hbs",
+    "./templates/partials/default/properties.hbs",
+    "./templates/partials/default/property.hbs",
+    "./templates/partials/default/propertyListItemArrayIfValue.hbs",
+    "./templates/partials/default/propertyListItemIfValue.hbs",
+    "./templates/partials/default/titleNamePath.hbs",
+    "./templates/partials/fields-as-headings/schemaField.hbs",
+    "./templates/partials/fields-as-headings/schemaFields.hbs",
+]
+
+for p in ps:
+    loadPartial(p)
+
+data = json.loads("./datapackage.json")
 
 # Render the template
-output = template(data, helpers=helpers, partials=partials)
+output = template(data, helpers={**helpers, **{"intlGet": intlGet}}, partials=partials)
 
 print(output)
