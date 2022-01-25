3c9795a39d65243cb95200ca11864c2945e9f3b7
Paul Girard <paul@ouestware.com>
2022-01-25 18:38:54 +0100

    ForeignKey error: metadata displayed in note FK error note are enhanced to help dat curator - fields from soruce and target table are added - values from source table - reference table name - Tests added from multi field FK validation and note format
    Those information are useful when curating from validation errors.
    fixes #973

--- tests/validate/test_package.py
+++ tests/validate/test_package.py
@@ -186,6 +186,24 @@ DESCRIPTOR_FK = {
     ],
 }
 
+MULTI_FK_RESSOURCE = {
+    "name": "travel_time",
+    "data": [["from", "to", "hours"], [1, 2, 1.5], [2, 3, 8], [3, 4, 18]],
+    "schema": {
+        "fields": [
+            {"name": "from", "type": "integer"},
+            {"name": "to", "type": "integer"},
+            {"name": "hours", "type": "number"},
+        ],
+        "foreignKeys": [
+            {
+                "fields": ["from", "to"],
+                "reference": {"resource": "cities", "fields": ["id", "next_id"]},
+            }
+        ],
+    },
+}
+
 
 def test_validate_package_schema_foreign_key_error():
     descriptor = deepcopy(DESCRIPTOR_FK)
@@ -227,6 +245,30 @@ def test_validate_package_schema_foreign_key_internal_resource_violation_non_exi
         [3, None, "foreign-key-error", ["2", "paris", "3"]],
         [4, None, "foreign-key-error", ["3", "rome", "4"]],
         [5, None, "foreign-key-error", ["4", "rio", ""]],
+    ]
+
+
+def test_validate_package_schema_multiple_foreign_key():
+    descriptor = deepcopy(DESCRIPTOR_FK)
+    descriptor["resources"].append(MULTI_FK_RESSOURCE)
+    report = validate(descriptor)
+    assert report.valid
+
+
+def test_validate_package_schema_multiple_foreign_key_resource_violation_non_existent():
+    descriptor = deepcopy(DESCRIPTOR_FK)
+    # remove London
+    del descriptor["resources"][0]["data"][1]
+    descriptor["resources"].append(MULTI_FK_RESSOURCE)
+    report = validate(descriptor)
+    assert report.flatten(["rowPosition", "fieldPosition", "code", "cells", "note"]) == [
+        [
+            2,
+            None,
+            "foreign-key-error",
+            ["1", "2", "1.5"],
+            'for "from, to": values "1, 2" not found in the lookup table "cities" as "id, next_id"',
+        ],
     ]
 
 
