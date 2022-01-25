87944daba23c5d523f2d18cd989ff9bc5033874e
roll <roll@users.noreply.github.com>
2022-01-25 13:14:01 +0300

    Added sqlDialect.basepath property (#982)
    * Added sqlDialect.basepath property
    
    * Fixed linting
    
    * Fixed implementation

--- frictionless/plugins/sql.py
+++ frictionless/plugins/sql.py
@@ -1,5 +1,6 @@
 import re
 from functools import partial
+from urllib.parse import urlsplit, urlunsplit
 from ..exception import FrictionlessException
 from ..metadata import Metadata
 from ..resource import Resource
@@ -70,6 +71,7 @@ class SqlDialect(Dialect):
         order_by? (str): order_by statement passed to SQL
         where? (str): where statement passed to SQL
         namespace? (str): SQL schema
+        basepath? (str): a basepath, for example, for SQLite path
 
     Raises:
         FrictionlessException: raise any error that occurs during the process
@@ -85,12 +87,14 @@ class SqlDialect(Dialect):
         order_by=None,
         where=None,
         namespace=None,
+        basepath=None,
     ):
         self.setinitial("table", table)
         self.setinitial("prefix", prefix)
         self.setinitial("order_by", order_by)
         self.setinitial("where", where)
         self.setinitial("namespace", namespace)
+        self.setinitial("basepath", basepath)
         super().__init__(descriptor)
 
     @Metadata.property
@@ -113,6 +117,10 @@ class SqlDialect(Dialect):
     def namespace(self):
         return self.get("namespace")
 
+    @Metadata.property
+    def basepath(self):
+        return self.get("basepath")
+
     # Metadata
 
     metadata_profile = {  # type: ignore
@@ -125,6 +133,7 @@ class SqlDialect(Dialect):
             "order_by": {"type": "string"},
             "where": {"type": "string"},
             "namespace": {"type": "string"},
+            "basepath": {"type": "string"},
         },
     }
 
@@ -197,6 +206,15 @@ class SqlStorage(Storage):
 
     def __init__(self, source, *, dialect=None):
         sa = helpers.import_from_plugin("sqlalchemy", plugin="sql")
+
+        # Create engine
+        if dialect and dialect.basepath:
+            url = urlsplit(source)
+            basepath = dialect.basepath
+            if isinstance(source, str) and source.startswith("sqlite"):
+                # Path for sqlite looks like this 'sqlite:///path' (unix/windows)
+                basepath = f"/{basepath}"
+            source = urlunsplit((url.scheme, basepath, url.path, url.query, url.fragment))
         engine = sa.create_engine(source) if isinstance(source, str) else source
 
         # Set attributes
