62bacb0df17feb61b18152c5042cb0277d4cd1e7
roll <roll@users.noreply.github.com>
2022-02-03 11:09:37 +0300

    Enable prev stuck tests (#989)

--- tests/test_package.py
+++ tests/test_package.py
@@ -878,7 +878,7 @@ def test_package_to_zip_resource_path(tmpdir):
     ]
 
 
-@pytest.mark.skip
+@pytest.mark.vcr
 def test_package_to_zip_resource_remote_path(tmpdir):
     path = os.path.join(tmpdir, "package.zip")
     source = Package(resources=[Resource(path=BASEURL % "data/table.csv")])
@@ -946,7 +946,7 @@ def test_package_to_zip_resource_multipart(tmpdir, database_url):
 # Validate
 
 
-@pytest.mark.skip
+@pytest.mark.vcr
 def test_package_external_profile():
     profile = "frictionless/assets/profiles/package/general.json"
     resource = Resource(name="table", path="data/table.csv")
@@ -954,7 +954,7 @@ def test_package_external_profile():
     assert package.metadata_valid
 
 
-@pytest.mark.skip
+@pytest.mark.vcr
 def test_package_external_profile_invalid_local():
     profile = "data/profiles/camtrap.json"
     resource = Resource(name="table", path="data/table.csv")
@@ -964,7 +964,7 @@ def test_package_external_profile_invalid_local():
         assert "required" in error.message
 
 
-@pytest.mark.skip
+@pytest.mark.vcr
 def test_package_external_profile_invalid_local_from_descriptor():
     profile = "data/profiles/camtrap.json"
     resource = Resource(name="table", path="data/table.csv")
@@ -974,7 +974,7 @@ def test_package_external_profile_invalid_local_from_descriptor():
         assert "required" in error.message
 
 
-@pytest.mark.skip
+@pytest.mark.vcr
 def test_package_external_profile_invalid_local_from_descriptor_unsafe():
     profile = "data/../data/profiles/camtrap.json"
     resource = Resource(name="table", path="data/table.csv")
@@ -984,7 +984,7 @@ def test_package_external_profile_invalid_local_from_descriptor_unsafe():
             package.metadata_errors
 
 
-@pytest.mark.skip
+@pytest.mark.vcr
 def test_package_external_profile_invalid_local_from_descriptor_unsafe_trusted():
     profile = "data/../data/profiles/camtrap.json"
     resource = Resource(name="table", path="data/table.csv")
