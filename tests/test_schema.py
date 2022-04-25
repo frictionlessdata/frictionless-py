0b6e17b35fa9b11df8d8495d5df4f807b795d7c2
shashi gharti <shashi.gharti@gmail.com>
2022-04-25 14:28:27 +0545

    fix for path error  in windows

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -491,7 +491,7 @@ def test_schema_tableschema_to_excel_584(tmpdir, zip_path):
     # zipfile.Path is introduced in Python3.8, and could make this cleaner:
     # xml_string = zipfile.Path(xlsx_path, zip_path).read_text()
 
-    schema_path = Path(__file__).parent / "fixtures/schema.yaml"
+    schema_path = Path(Path(__file__).parent, "fixtures/schema.yaml")
     schema = Schema(safe_load(schema_path.read_text()))
     xlsx_tmp_path = os.path.join(tmpdir, "template.xlsx")
     schema.to_excel_template(xlsx_tmp_path)
@@ -504,11 +504,11 @@ def test_schema_tableschema_to_excel_584(tmpdir, zip_path):
     # so we need to use an outside library.
     yattag = helpers.import_from_plugin("yattag", plugin="excel")
     pretty_xml = yattag.indent(xml_string)
-    pretty_xml_fixture_path = (
-        Path(__file__).parent / "fixtures/output-unzipped" / zip_path
+    pretty_xml_fixture_path = Path(
+        Path(__file__).parent, "fixtures/output-unzipped", zip_path
     )
 
-    pretty_xml_tmp_path = Path(tmpdir) / Path(zip_path).name
+    pretty_xml_tmp_path = Path(Path(tmpdir), Path(zip_path).name)
     pretty_xml_tmp_path.write_text(pretty_xml)
 
     assert pretty_xml.strip() == pretty_xml_fixture_path.read_text().strip()
