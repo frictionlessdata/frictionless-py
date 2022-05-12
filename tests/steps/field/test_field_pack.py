ce8f192bfb2a16c96380c08880427f0507db398e
Shashi Gharti <shashi.gharti@gmail.com>
2022-05-12 12:47:21 -0700

    Added `steps.field_merge` and `steps.field_pack` (#1070)
    * * Added field-merge and field-pack step
    * tests for field-merge and field-pack
    
    * removed extra file that was added
    
    * updated PR
    
    * Added field type
    * Added doc blocks
    * Reorganized code to respective steps file
    * Fixed tests for steps merge and pack

---
+++ tests/steps/field/test_field_pack.py
@@ -0,0 +1,74 @@
+from frictionless import Resource, transform, steps
+
+
+def test_step_field_pack_907():
+    source = Resource("data/transform.csv")
+    target = transform(
+        source,
+        steps=[steps.field_pack(name="details", from_names=["name", "population"])],
+    )
+    assert target.schema == {
+        "fields": [
+            {"name": "id", "type": "integer"},
+            {"name": "details", "type": "array"},
+        ]
+    }
+    assert target.read_rows()[0] == {
+        "id": 1,
+        "details": ["germany", "83"],
+    }
+
+
+def test_step_field_pack_header_preserve_907():
+    source = Resource("data/transform.csv")
+    target = transform(
+        source,
+        steps=[
+            steps.field_pack(
+                name="details", from_names=["name", "population"], preserve=True
+            )
+        ],
+    )
+    assert target.schema == {
+        "fields": [
+            {"name": "id", "type": "integer"},
+            {"name": "name", "type": "string"},
+            {"name": "population", "type": "integer"},
+            {"name": "details", "type": "array"},
+        ]
+    }
+    assert target.read_rows()[0] == {
+        "id": 1,
+        "name": "germany",
+        "population": 83,
+        "details": ["germany", "83"],
+    }
+
+
+def test_step_field_pack_object_907():
+    source = Resource("data/transform.csv")
+    target = transform(
+        source,
+        steps=[
+            steps.field_pack(
+                name="details",
+                from_names=["name", "population"],
+                field_type="object",
+                preserve=True,
+            )
+        ],
+    )
+    assert target.schema == {
+        "fields": [
+            {"name": "id", "type": "integer"},
+            {"name": "name", "type": "string"},
+            {"name": "population", "type": "integer"},
+            {"name": "details", "type": "object"},
+        ]
+    }
+    assert target.read_rows()[0] == {
+        "id": 1,
+        "name": "germany",
+        "population": 83,
+        "details": {"name": "germany", "population": "83"},
+    }
