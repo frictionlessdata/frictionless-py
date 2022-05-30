e04d4830f19e94b3816209007e6ffa01ee4b58ff
shashi gharti <shashi.gharti@gmail.com>
2022-05-30 11:31:13 +0545

    apply formatting

--- tests/actions/validate/test_package.py
+++ tests/actions/validate/test_package.py
@@ -532,7 +532,7 @@ def test_validate_package_with_resource_data_is_a_string_issue_977():
     assert report.flatten() == [
         [None, None, None, "package-error"],
     ]
-    
+
 
 def test_validate_package_single_resource_221():
     report = validate("data/datapackage.json", resource_name="number-two")
