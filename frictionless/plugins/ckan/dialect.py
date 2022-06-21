810bbd59859f0b8a760286fe39cf4625e88a9547
roll <eskarev@gmail.com>
2022-06-21 16:39:06 +0300

    Migrated CkanDialect

--- frictionless/plugins/ckan/dialect.py
+++ frictionless/plugins/ckan/dialect.py
@@ -1,77 +1,32 @@
-# type: ignore
-from ...metadata import Metadata
+from typing import Optional, List
 from ...dialect import Dialect
 
 
 class CkanDialect(Dialect):
-    """Ckan dialect representation
+    """Ckan dialect representation"""
 
-    API      | Usage
-    -------- | --------
-    Public   | `from frictionless.plugins.ckan import CkanDialect`
+    # Properties
 
-    Parameters:
-        descriptor? (str|dict): descriptor
-        resource? (str): resource
-        dataset? (str): dataset
-        apikey? (str): apikey
-        fields? (array): limit ckan query to certain fields
-        limit? (int): limit number of returned entries
-        sort? (str): sort returned entries, e.g. by date descending: `date desc`
-        filters? (dict): filter data, e.g. field with value: `{ "key": "value" }`
+    resource: str
+    """TODO: add docs"""
 
-    Raises:
-        FrictionlessException: raise any error that occurs during the process
-    """
+    dataset: str
+    """TODO: add docs"""
 
-    def __init__(
-        self,
-        descriptor=None,
-        *,
-        dataset=None,
-        resource=None,
-        apikey=None,
-        fields=None,
-        limit=None,
-        sort=None,
-        filters=None,
-    ):
-        self.setinitial("resource", resource)
-        self.setinitial("dataset", dataset)
-        self.setinitial("apikey", apikey)
-        self.setinitial("fields", fields)
-        self.setinitial("limit", limit)
-        self.setinitial("sort", sort)
-        self.setinitial("filters", filters)
-        super().__init__(descriptor)
+    apikey: Optional[str] = None
+    """TODO: add docs"""
 
-    @Metadata.property
-    def resource(self):
-        return self.get("resource")
+    fields: Optional[List[str]] = None
+    """TODO: add docs"""
 
-    @Metadata.property
-    def dataset(self):
-        return self.get("dataset")
+    limit: Optional[int] = None
+    """TODO: add docs"""
 
-    @Metadata.property
-    def apikey(self):
-        return self.get("apikey")
+    sort: Optional[str] = None
+    """TODO: add docs"""
 
-    @Metadata.property
-    def fields(self):
-        return self.get("fields")
-
-    @Metadata.property
-    def limit(self):
-        return self.get("limit")
-
-    @Metadata.property
-    def sort(self):
-        return self.get("sort")
-
-    @Metadata.property
-    def filters(self):
-        return self.get("filters")
+    filters: Optional[dict] = None
+    """TODO: add docs"""
 
     # Metadata
 
