e16513131f19e588ecef19d4712400225b75ae92
roll <eskarev@gmail.com>
2022-06-22 09:42:06 +0300

    Added dialect.controls

--- frictionless/dialect/dialect.py
+++ frictionless/dialect/dialect.py
@@ -1,16 +1,44 @@
+from __future__ import annotations
+from typing import List
+from dataclasses import dataclass, field
 from ..metadata2 import Metadata2
 from .describe import describe
 from .validate import validate
+from .. import settings
 from .. import errors
 
 
-# TODO: controls should have code
+@dataclass
 class Dialect(Metadata2):
     """Dialect representation"""
 
     describe = describe
     validate = validate
 
+    # Properties
+
+    header_rows: List[int] = field(default_factory=lambda: settings.DEFAULT_HEADER_ROWS)
+    """TODO: add docs"""
+
+    header_join: str = settings.DEFAULT_HEADER_JOIN
+    """TODO: add docs"""
+
+    header_case: bool = settings.DEFAULT_HEADER_CASE
+    """TODO: add docs"""
+
+    controls: List[Control] = field(default_factory=list)
+    """TODO: add docs"""
+
     # Metadata
 
     metadata_Error = errors.DialectError
+
+
+class Control(Metadata2):
+    """Control representation"""
+
+    code: str
+
+    # Metadata
+
+    metadata_Error = errors.ControlError
