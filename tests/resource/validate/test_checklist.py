fed169369a639464aa35501273e0751aea3d01ff
roll <eskarev@gmail.com>
2022-07-04 09:02:28 +0300

    Recovered validate resource checklist

--- tests/resource/validate/test_checklist.py
+++ tests/resource/validate/test_checklist.py
@@ -1,7 +1,4 @@
-import pytest
 from frictionless import Resource, Checklist
-
-pytestmark = pytest.mark.skip
 
 
 # General
@@ -12,7 +9,7 @@ def test_resource_validate_bound_checklist():
     resource = Resource("data/invalid.csv", checklist=checklist)
     report = resource.validate()
     assert report.task.scope == ["blank-label", "blank-row"]
-    assert report.flatten(["rowPosition", "fieldPosition", "code"]) == [
+    assert report.flatten(["rowNumber", "fieldNumber", "code"]) == [
         [None, 3, "blank-label"],
         [4, None, "blank-row"],
     ]
