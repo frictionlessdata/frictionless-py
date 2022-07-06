44fe1209a02ce8d4e8f01d7c2d5c473ea6056849
roll <eskarev@gmail.com>
2022-07-06 10:05:14 +0300

    Recovered table tests

--- tests/table/test_row.py
+++ tests/table/test_row.py
@@ -38,22 +38,24 @@ def test_to_str_with_doublequotes():
     assert rows[1].to_str() == '2,"german,GE"'
 
 
-@pytest.mark.skip
 def test_to_dict_with_json_null_values_issue_519():
     source = b"value\n2020-01-01\n\n2020-03-03"
     process = lambda row: row.to_dict(json=True)
-    extract(source, format="csv", process=process) == [
+    resource = Resource(source, format="csv")
+    result = resource.extract(process=process)
+    assert result == [
         {"value": "2020-01-01"},
         {"value": None},
         {"value": "2020-03-03"},
     ]
 
 
-@pytest.mark.skip
 def test_to_list_with_json_null_values_issue_519():
     source = b"value\n2020-01-01\n\n2020-03-03"
     process = lambda row: row.to_list(json=True)
-    extract(source, format="csv", process=process) == [
+    resource = Resource(source, format="csv")
+    result = resource.extract(process=process)
+    assert result == [
         ["2020-01-01"],
         [None],
         ["2020-03-03"],
