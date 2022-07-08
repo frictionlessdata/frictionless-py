866ac4198866ff09374d999f8f2d24307109426c
roll <eskarev@gmail.com>
2022-07-08 19:37:04 +0300

    Recovered resource.infer tests

--- tests/resource/test_infer.py
+++ tests/resource/test_infer.py
@@ -5,20 +5,21 @@ from frictionless import Resource, helpers
 # General
 
 
-@pytest.mark.skip
 @pytest.mark.skipif(helpers.is_platform("windows"), reason="Fix on Windows")
 def test_resource_infer():
     resource = Resource(path="data/table.csv")
     resource.infer(stats=True)
     assert resource.metadata_valid
-    assert resource == {
-        "path": "data/table.csv",
-        "profile": "tabular-data-resource",
+    assert resource.to_descriptor() == {
         "name": "table",
+        "path": "data/table.csv",
+        "type": "table",
         "scheme": "file",
         "format": "csv",
         "hashing": "md5",
         "encoding": "utf-8",
+        "mediatype": "text/csv",
+        "dialect": {"controls": [{"code": "local"}, {"code": "csv"}]},
         "schema": {
             "fields": [
                 {"name": "id", "type": "integer"},
@@ -34,20 +35,21 @@ def test_resource_infer():
     }
 
 
-@pytest.mark.skip
 @pytest.mark.skipif(helpers.is_platform("windows"), reason="Fix on Windows")
 def test_resource_infer_source_non_tabular():
     resource = Resource(path="data/text.txt")
     resource.infer(stats=True)
     assert resource.metadata_valid
-    assert resource == {
+    assert resource.to_descriptor() == {
         "name": "text",
         "path": "data/text.txt",
-        "profile": "data-resource",
+        "type": "file",
         "scheme": "file",
         "format": "txt",
         "hashing": "md5",
         "encoding": "utf-8",
+        # TODO: improve in detector.detect_resource
+        "mediatype": "application/txt",
         "stats": {
             "hash": "e1cbb0c3879af8347246f12c559a86b5",
             "bytes": 5,
@@ -55,7 +57,6 @@ def test_resource_infer_source_non_tabular():
     }
 
 
-@pytest.mark.skip
 def test_resource_infer_from_path():
     resource = Resource("data/table.csv")
     resource.infer(stats=True)
@@ -63,7 +64,9 @@ def test_resource_infer_from_path():
     assert resource.path == "data/table.csv"
 
 
-@pytest.mark.skip
+# Bugs
+
+
 def test_resource_infer_not_slugified_name_issue_531():
     resource = Resource("data/Table With Data.csv")
     resource.infer(stats=True)
