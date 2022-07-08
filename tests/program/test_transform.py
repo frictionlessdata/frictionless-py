c32497a5a20cb26dc3c8eeaf62056c94eedea1e6
roll <eskarev@gmail.com>
2022-07-08 10:58:54 +0300

    Recovered program tests

--- tests/program/test_transform.py
+++ tests/program/test_transform.py
@@ -5,11 +5,7 @@ from frictionless import program, helpers
 runner = CliRunner()
 
 
-# General
-# TODO: rework on the new pipeline usage
-
-
-@pytest.mark.skip
+@pytest.mark.xfail(reason="Rework for the new Pipeline")
 @pytest.mark.skipif(helpers.is_platform("windows"), reason="Fix on Windows")
 def test_program_transform():
     result = runner.invoke(program, "transform data/pipeline.yaml")
@@ -17,14 +13,13 @@ def test_program_transform():
     assert result.stdout.count("success: data/pipeline.yaml")
 
 
-@pytest.mark.skip
 def test_program_transform_error_not_found():
     result = runner.invoke(program, "transform data/bad.yaml")
     assert result.exit_code == 1
     assert result.stdout.count("[Errno 2]") and result.stdout.count("data/bad.yaml")
 
 
-@pytest.mark.skip
+@pytest.mark.xfail(reason="Rework for the new Pipeline")
 def test_program_transform_error_not_found_source_issue_814():
     result = runner.invoke(program, "transform data/issue-814.yaml")
     assert result.exit_code == 1
