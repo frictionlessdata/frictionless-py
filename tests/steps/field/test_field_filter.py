699c30978f29037f284e4e9c80571cfc61462710
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2022-07-08 19:43:27 +0200

    parametrize test_step_field_filter

--- tests/steps/field/test_field_filter.py
+++ tests/steps/field/test_field_filter.py
@@ -2,69 +2,121 @@ import pytest
 
 
 from frictionless import Resource, transform, steps
-from frictionless.exception import FrictionlessException
 
 
 # General
 
 
-def test_step_field_filter():
-    source = Resource(path="data/transform.csv")
+@pytest.mark.parametrize(
+    "scenario",
+    [
+        {
+            "test_id": "idem",
+            "csv_path": "data/transform.csv",
+            "names": ["id", "name", "population"],
+            "expected_schema": {
+                "fields": [
+                    {"name": "id", "type": "integer"},
+                    {"name": "name", "type": "string"},
+                    {"name": "population", "type": "integer"},
+                ]
+            },
+            "expected_rows": [
+                {"id": 1, "name": "germany", "population": 83},
+                {"id": 2, "name": "france", "population": 66},
+                {"id": 3, "name": "spain", "population": 47},
+            ],
+        },
+        {
+            "test_id": "reverse_field_order",
+            "csv_path": "data/transform.csv",
+            "names": ["population", "name", "id"],
+            "expected_schema": {
+                "fields": [
+                    {"name": "population", "type": "integer"},
+                    {"name": "name", "type": "string"},
+                    {"name": "id", "type": "integer"},
+                ]
+            },
+            "expected_rows": [
+                {"population": 83, "name": "germany", "id": 1},
+                {"population": 66, "name": "france", "id": 2},
+                {"population": 47, "name": "spain", "id": 3},
+            ],
+        },
+        {
+            "test_id": "remove_last_column",
+            "csv_path": "data/transform.csv",
+            "names": ["id", "name"],
+            "expected_schema": {
+                "fields": [
+                    {"name": "id", "type": "integer"},
+                    {"name": "name", "type": "string"},
+                ]
+            },
+            "expected_rows": [
+                {"id": 1, "name": "germany"},
+                {"id": 2, "name": "france"},
+                {"id": 3, "name": "spain"},
+            ],
+        },
+        {
+            "test_id": "changed_field_order",
+            "csv_path": "data/transform.csv",
+            "names": ["name", "id"],
+            "expected_schema": {
+                "fields": [
+                    {"name": "name", "type": "string"},
+                    {"name": "id", "type": "integer"},
+                ]
+            },
+            "expected_rows": [
+                {"name": "germany", "id": 1},
+                {"name": "france", "id": 2},
+                {"name": "spain", "id": 3},
+            ],
+        },
+        {
+            "test_id": "missing_name",
+            "csv_path": "data/transform.csv",
+            "names": ["name", "nonexistent"],
+            "expected_schema": {
+                "fields": [
+                    {"name": "name", "type": "string"},
+                ]
+            },
+            "expected_rows": [
+                {"name": "germany"},
+                {"name": "france"},
+                {"name": "spain"},
+            ],
+        },
+        {
+            "test_id": "no_fields",
+            "csv_path": "data/transform.csv",
+            "names": [],
+            "expected_schema": {"fields": []},
+            "expected_rows": [
+                {},
+                {},
+                {},
+            ],
+        },
+    ],
+    ids=lambda scenario: scenario["test_id"],
+)
+def test_step_field_filter(scenario):
+    csv_path = scenario["csv_path"]
+    names = scenario["names"]
+    expected_schema = scenario["expected_schema"]
+    expected_rows = scenario["expected_rows"]
+
+    source = Resource(path=csv_path)
     target = transform(
         source,
         steps=[
-            steps.field_filter(names=["id", "name"]),
+            steps.field_filter(names=names),
         ],
     )
-    assert target.schema == {
-        "fields": [
-            {"name": "id", "type": "integer"},
-            {"name": "name", "type": "string"},
-        ]
-    }
-    assert target.read_rows() == [
-        {"id": 1, "name": "germany"},
-        {"id": 2, "name": "france"},
-        {"id": 3, "name": "spain"},
-    ]
-
-
-def test_step_field_filter_changed_field_order():
-    source = Resource(path="data/transform.csv")
-    target = transform(
-        source,
-        steps=[
-            steps.field_filter(names=["name", "id"]),
-        ],
-    )
-    assert target.schema == {
-        "fields": [
-            {"name": "name", "type": "string"},
-            {"name": "id", "type": "integer"},
-        ]
-    }
-    assert target.read_rows() == [
-        {"name": "germany", "id": 1},
-        {"name": "france", "id": 2},
-        {"name": "spain", "id": 3},
-    ]
-
-
-def test_step_field_filter_missing_name():
-    source = Resource(path="data/transform.csv")
-    target = transform(
-        source,
-        steps=[
-            steps.field_filter(names=["name", "nonexistent"]),
-        ],
-    )
-    assert target.schema == {
-        "fields": [
-            {"name": "name", "type": "string"},
-        ]
-    }
-    assert target.read_rows() == [
-        {"name": "germany"},
-        {"name": "france"},
-        {"name": "spain"},
-    ]
+    assert target.schema == expected_schema
+    assert target.read_rows() == expected_rows
