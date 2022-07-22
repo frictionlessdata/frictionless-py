8ec78621ea44a6dfe15cf66d06ef2a943ea3f5e3
shashi gharti <shashi.gharti@gmail.com>
2022-07-22 20:55:08 +0545

    Added doc blocks to the steps classes
    Added docblocks to steps classes

--- frictionless/steps/cell/cell_convert.py
+++ frictionless/steps/cell/cell_convert.py
@@ -11,20 +11,123 @@ from ...pipeline import Step
 
 @attrs.define(kw_only=True)
 class cell_convert(Step):
-    """Convert cell"""
+    """Convert cell
+    
+    Converts cell values of one or more fields using arbitrary functions, method
+    invocations or dictionary translations. 
+
+    Parameters
+    ----------
+        type  : step identifier
+        value : value to replace in cells.
+        function : arbitrary function, data method or dictionary translations.
+        field_name : field name to apply function to.
+
+    Methods
+    -------
+        transform_resource(resource):
+            converts cell value of a resource using arbitrary function, data method 
+            or dictionary translations
+
+    Examples
+    --------
+    >>> from frictionless import Resource, Pipeline, steps
+    >>> source = Resource(path="data/transform.csv")
+        +----+-----------+------------+
+        | id | name      | population |
+        +====+===========+============+
+        |  1 | 'germany' |         83 |
+        +----+-----------+------------+
+        |  2 | 'france'  |         66 |
+        +----+-----------+------------+
+        |  3 | 'spain'   |         47 |
+        +----+-----------+------------+
+    >>> # replacing value
+    >>> pipeline = Pipeline(
+            steps=[
+                steps.cell_convert(field_name='population', value="100"),
+            ],
+        )
+    >>> target = source.transform(pipeline)
+    >>> print(target.to_view()) 
+        +----+-----------+------------+
+        | id | name      | population |
+        +====+===========+============+
+        |  1 | 'germany' |        100 |
+        +----+-----------+------------+
+        |  2 | 'france'  |        100 |
+        +----+-----------+------------+
+        |  3 | 'spain'   |        100 |
+        +----+-----------+------------+
+
+    >>> # using lamda function
+    >>> pipeline = Pipeline(
+            steps=[
+                steps.cell_convert(function=lambda v: v*2, field_name='population'),
+            ],
+        )
+    >>> target = source.transform(pipeline)
+    >>> print(target.to_view())
+        +----+-----------+------------+
+        | id | name      | population |
+        +====+===========+============+
+        |  1 | 'germany' |     100100 |
+        +----+-----------+------------+
+        |  2 | 'france'  |     100100 |
+        +----+-----------+------------+
+        |  3 | 'spain'   |     100100 |
+        +----+-----------+------------+
+    
+    >>> # using method of data value
+    >>> pipeline = Pipeline(
+            steps=[
+                steps.cell_convert(function='upper', field_name='name'),
+            ],
+        )
+    >>> target = source.transform(pipeline)
+    >>> print(target.to_view())
+        +----+-----------+------------+
+        | id | name      | population |
+        +====+===========+============+
+        |  1 | 'GERMANY' |     100100 |
+        +----+-----------+------------+
+        |  2 | 'FRANCE'  |     100100 |
+        +----+-----------+------------+
+        |  3 | 'SPAIN'   |     100100 |
+        +----+-----------+------------+
+    
+    >>> # using a dictionary
+    >>> pipeline = Pipeline(
+            steps=[
+                steps.cell_convert(field_name='name', function = {'GERMANY': 'Z', 'B': 'Y'}),
+            ],
+        )
+    >>> target = source.transform(pipeline)
+    >>> print(target.to_view())
+        +----+----------+------------+-----------------+
+        | id | name     | population | avg_age         |
+        +====+==========+============+=================+
+        |  1 | 'Z'      |     100100 | Decimal('30.5') |
+        +----+----------+------------+-----------------+
+        |  2 | 'FRANCE' |     100100 | Decimal('30.0') |
+        +----+----------+------------+-----------------+
+        |  3 | 'SPAIN'  |     100100 | Decimal('40.0') |
+        +----+----------+------------+-----------------+
+
+    """
 
     type = "cell-convert"
 
     # State
 
     value: Optional[Any] = None
-    """TODO: add docs"""
+    """Value to replace in the field cell"""
 
     function: Optional[Any] = None
-    """TODO: add docs"""
+    """Function/Data method/Dictionary to apply to the column"""
 
     field_name: Optional[str] = None
-    """TODO: add docs"""
+    """Name of the field to apply the function"""
 
     # Transform
 
