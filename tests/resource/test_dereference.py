86b066dbf7ea3e16a0cc74947dedf1da032a547a
roll <eskarev@gmail.com>
2022-08-01 16:40:02 +0300

    Updated dereference test

--- tests/resource/test_dereference.py
+++ tests/resource/test_dereference.py
@@ -14,21 +14,16 @@ def test_resource_dereference():
     resource.infer(stats=True)
     assert resource.to_descriptor() == {
         "name": "table",
-        "path": "data/table.csv",
         "type": "table",
+        "path": "data/table.csv",
         "scheme": "file",
         "format": "csv",
         "encoding": "utf-8",
         "mediatype": "text/csv",
-        "dialect": {
-            "controls": [
-                {"code": "local"},
-                {"code": "csv"},
-            ]
-        },
         "schema": "data/schema.json",
         "stats": {
-            "hash": "6c2c61dd9b0e9c6876139a449ed87933",
+            "md5": "6c2c61dd9b0e9c6876139a449ed87933",
+            "sha256": "a1fd6c5ff3494f697874deeb07f69f8667e903dd94a7bc062dd57550cea26da8",
             "bytes": 30,
             "fields": 2,
             "rows": 2,
