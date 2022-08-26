030fd230b0e75ed011ed9ebd151fbf12301d4547
roll <eskarev@gmail.com>
2022-08-26 16:47:29 +0300

    Load tsv by default from github

--- frictionless/portals/github/control.py
+++ frictionless/portals/github/control.py
@@ -5,7 +5,7 @@ from typing import Optional, List
 from ...dialect import Control
 
 
-DEFAULT_FORMATS = ["csv", "xlsx", "xls"]
+DEFAULT_FORMATS = ["csv", "tsv", "xlsx", "xls"]
 DEFAULT_PER_PAGE = 30
 
 
