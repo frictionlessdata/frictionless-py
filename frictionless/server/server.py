49accf5628f7cc6c29782fb7ffe56ae4d97ea1ff
roll <eskarev@gmail.com>
2022-08-30 11:22:50 +0300

    Added version to server

--- frictionless/server/server.py
+++ frictionless/server/server.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 from ..platform import platform
+from .. import settings
 
 
-# TODO: split on validate/resource|package|inquiry|etc
-
-
-server = platform.fastapi.FastAPI(title="Frictionless API")
+server = platform.fastapi.FastAPI(title="Frictionless API", version=settings.VERSION)
