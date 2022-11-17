5ee05b94bca8b342a9ee88cd2b34711b2cd8a2c3
shashi gharti <shashi.gharti@gmail.com>
2022-11-17 07:54:34 +0545

    Added api endpoints for move file feature
    Added function to list folders, create directory and move file

---
+++ frictionless/server/projects/create_directory.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import Request
+from ..project import Project
+from ..router import router
+
+
+class ProjectCreateDirectoryProps(BaseModel):
+    session: Optional[str]
+    directoryname: str
+
+
+@router.post("/project/create-directory")
+def server_project_create_directory(request: Request, props: ProjectCreateDirectoryProps):
+    print("props", props)
+    config = request.app.config
+    project = Project(config, session=props.session)
+    newdirectory = project.create_directory(props.directoryname)
+    return {"path": newdirectory}
