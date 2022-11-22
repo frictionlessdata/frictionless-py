f1d2f0405b481efd9d340e657c2d90d7dde6314a
roll <roll@users.noreply.github.com>
2022-11-22 17:24:14 +0300

    Moved jsonschema to its own module (#1327)

---
+++ frictionless/formats/jsonschema/mapper.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from typing import Dict, Any
+from ...schema import Schema, Field
+
+
+class JsonschemaMapper:
+    """ERD Mapper"""
+
+    # Write
+
+    def read_schema(self, profile: Dict[str, Any]) -> Schema:
+        schema = Schema()
+        required = profile.get("required", [])
+        assert isinstance(required, list)
+        properties = profile.get("properties", {})
+        assert isinstance(properties, dict)
+        for name, prop in properties.items():
+
+            # Type
+            type = prop.get("type", "any")
+            assert isinstance(type, str)
+            if type not in ["string", "integer", "number", "boolean", "object", "array"]:
+                type = "any"
+
+            # Field
+            assert isinstance(name, str)
+            assert isinstance(prop, dict)
+            field = Field.from_descriptor({"type": type, "name": name})
+            schema.add_field(field)
+
+            # Description
+            description = prop.get("description")
+            if description:
+                assert isinstance(description, str)
+                field.description = description
+
+            # Required
+            if name in required:
+                field.required = True
+
+        return schema
