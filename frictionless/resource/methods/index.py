99366026b46d646d26d6309f0f04e7d76d8e49e4
roll <eskarev@gmail.com>
2023-01-07 11:56:49 +0000

    Split resource.index by database engine

--- frictionless/resource/methods/index.py
+++ frictionless/resource/methods/index.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+from frictionless.exception import FrictionlessException
 from ...platform import platform
 
 if TYPE_CHECKING:
@@ -9,26 +10,36 @@ if TYPE_CHECKING:
 
 def index(self: Resource, database_url: str, *, table_name: str) -> Report:
     """Index resource into a database"""
-    with self, platform.psycopg.connect(database_url) as connection:
-        engine = platform.sqlalchemy.create_engine(database_url)
-        mapper = platform.frictionless_formats.sql.SqlMapper(engine)
-        sql = platform.sqlalchemy_schema
+    sa = platform.sqlalchemy
+    url = sa.engine.make_url(database_url)
 
-        # Write metadata
-        table = mapper.write_schema(self.schema, table_name=table_name)
-        with connection.cursor() as cursor:
-            cursor.execute(str(sql.DropTable(table, bind=engine, if_exists=True)))  # type: ignore
-            cursor.execute(str(sql.CreateTable(table, bind=engine)))  # type: ignore
+    # Postgresql
+    if url.drivername.startswith("postgresql"):
+        engine = sa.create_engine(database_url)
+        with self, platform.psycopg.connect(database_url) as connection:
+            engine = platform.sqlalchemy.create_engine(database_url)
+            mapper = platform.frictionless_formats.sql.SqlMapper(engine)
+            sql = platform.sqlalchemy_schema
 
-        # Write data
-        with connection.cursor() as cursor:
-            with cursor.copy('COPY "%s" FROM STDIN' % table_name) as copy:  # type: ignore
+            # Write metadata
+            table = mapper.write_schema(self.schema, table_name=table_name)
+            with connection.cursor() as cursor:
+                cursor.execute(str(sql.DropTable(table, bind=engine, if_exists=True)))  # type: ignore
+                cursor.execute(str(sql.CreateTable(table, bind=engine)))  # type: ignore
 
-                # Write row
-                def callback(row):
-                    cells = mapper.write_row(row)
-                    copy.write_row(cells)
+            # Write data
+            with connection.cursor() as cursor:
+                with cursor.copy('COPY "%s" FROM STDIN' % table_name) as copy:  # type: ignore
 
-                # Return report
-                report = self.validate(callback=callback)
-                return report
+                    # Write row
+                    def callback(row):
+                        cells = mapper.write_row(row)
+                        copy.write_row(cells)
+
+                    # Return report
+                    report = self.validate(callback=callback)
+                    return report
+
+    # Not supported
+    else:
+        raise FrictionlessException(f"not supported database: {url.drivername}")
