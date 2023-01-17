481854acb40d389d289177db2f366354fb713d69
roll <roll@users.noreply.github.com>
2023-01-17 10:56:20 +0000

    Support `dialect.skip_blank_rows` (#1387)
    * Added `dialect.skip_blank_rows`
    
    * Updated the docs
    
    * Improved tests
    
    * Updated docs

--- tests/dialect/test_general.py
+++ tests/dialect/test_general.py
@@ -1,5 +1,5 @@
 import pytest
-from frictionless import Dialect, FrictionlessException
+from frictionless import Resource, Dialect, FrictionlessException
 
 
 # General
@@ -21,3 +21,23 @@ def test_dialect_bad_property():
     assert error.note == "descriptor is not valid"
     assert reasons[0].type == "dialect-error"
     assert reasons[0].note == "'bad' is not of type 'array' at property 'headerRows'"
+
+
+# Blank Rows
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "data/blank-rows.csv",
+        "data/blank-rows-multiple.csv",
+        "data/blank-rows-no-fields.csv",
+    ],
+)
+def test_dialect_skip_blank_rows(path):
+    dialect = Dialect(skip_blank_rows=True)
+    with Resource(path, dialect=dialect) as resource:
+        assert resource.read_rows() == [
+            {"id": 1101, "name": "John", "age": 30},
+            {"id": 1102, "name": "Julie", "age": 26},
+        ]
