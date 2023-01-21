cad2d109831c1c51522858f5743b1c81296e22b4
roll <eskarev@gmail.com>
2023-01-21 11:16:37 +0000

    Added standards.pipeline

--- frictionless/standards/__init__.py
+++ frictionless/standards/__init__.py
@@ -1,4 +1,5 @@
 from .checklist import IChecklist, ICheck
 from .dialect import IDialect, IControl
+from .pipeline import IPipeline, IStep
 from .schema import ISchema, IField
 from .standard import Standard
