4b3c9ee8d47732e8bf8239569c1b9024162d6d45
roll <eskarev@gmail.com>
2023-01-31 17:00:51 +0000

    Improved 'file/create_folder'

--- frictionless/server/file/create_folder.py
+++ frictionless/server/file/create_folder.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import Request
@@ -7,7 +8,8 @@ from ..router import router
 
 class Props(BaseModel):
     session: Optional[str]
-    path: str
+    name: str
+    folder: Optional[str]
 
 
 class Result(BaseModel):
@@ -17,5 +19,8 @@ class Result(BaseModel):
 @router.post("/file/create-folder")
 def server_file_create_folder(request: Request, props: Props) -> Result:
     project: Project = request.app.get_project(props.session)
-    path = project.file_create_folder(props.path)
+    path = props.name
+    if props.folder:
+        path = str(Path(props.folder) / path)
+    path = project.file_create_folder(path)
     return Result(path=path)
