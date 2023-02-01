745e4f847eeefc38f1aed0e53688e4f891ccda35
roll <eskarev@gmail.com>
2023-02-01 19:25:24 +0000

    Fixed endpoint

--- frictionless/server/file/rename.py
+++ frictionless/server/file/rename.py
@@ -15,8 +15,8 @@ class Result(BaseModel):
     path: str
 
 
-@router.post("/file/move")
-def server_file_move(request: Request, props: Props) -> Result:
+@router.post("/file/rename")
+def server_file_rename(request: Request, props: Props) -> Result:
     project: Project = request.app.get_project(props.session)
     path = project.file_rename(props.path, name=props.name)
     return Result(path=path)
