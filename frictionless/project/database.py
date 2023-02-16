bb10b6490d80fc84f8a03a276f3a3d6c767eeaad
roll <eskarev@gmail.com>
2023-02-16 10:58:01 +0000

    Rebased Database on proper sq code

--- frictionless/project/database.py
+++ frictionless/project/database.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 import json
 from typing import TYPE_CHECKING, Optional, List
 from datetime import datetime
-from functools import cached_property
 from ..schema import Schema
 from ..platform import platform
 from .interfaces import IRecord, IRecordItem, ITable, IQueryData, IFieldItem
 
 if TYPE_CHECKING:
-    from sqlalchemy import Table
+    from sqlalchemy import Table, MetaData
+    from sqlalchemy.engine import Engine
+    from ..formats.sql import SqlMapper
     from ..resource import Resource
 
 
@@ -19,97 +20,90 @@ BUFFER_SIZE = 1000
 
 class Database:
     database_url: str
+    engine: Engine
+    mapper: SqlMapper
+    metadata: MetaData
     project: Table
     records: Table
 
     def __init__(self, database_url: str):
         sa = platform.sqlalchemy
         self.database_url = database_url
+        self.engine = sa.create_engine(self.database_url)
+        self.mapper = platform.frictionless_formats.sql.SqlMapper(self.engine)
+        with self.engine.begin() as conn:
+            self.metadata = sa.MetaData()
+            self.metadata.reflect(conn)
 
-        # Ensure project table
-        self.project = self.metadata.tables.get(PROJECT_IDENTIFIER)
-        if self.project is None:
-            self.project = sa.Table(
-                PROJECT_IDENTIFIER,
-                self.metadata,
-                sa.Column("config", sa.Text),
-            )
-            self.project.create(self.connection)
+            # Ensure project table
+            self.project = self.metadata.tables.get(PROJECT_IDENTIFIER)
+            if self.project is None:
+                self.project = sa.Table(
+                    PROJECT_IDENTIFIER,
+                    self.metadata,
+                    sa.Column("config", sa.Text),
+                )
+                self.metadata.create_all(conn, tables=[self.project])
 
-        # Ensure records table
-        # TODO: move some columns to metadata/other-name json column to avoid migrations
-        self.records = self.metadata.tables.get(RECORDS_IDENTIFIER)
-        if self.records is None:
-            self.records = sa.Table(
-                RECORDS_IDENTIFIER,
-                self.metadata,
-                sa.Column("path", sa.Text, primary_key=True),
-                sa.Column("type", sa.Text),
-                sa.Column("updated", sa.DateTime),
-                sa.Column("tableName", sa.Text, unique=True, nullable=True),
-                sa.Column("resource", sa.Text),
-                sa.Column("report", sa.Text),
-            )
-            self.records.create(self.connection)
-
-    @cached_property
-    def engine(self):
-        return platform.sqlalchemy.create_engine(self.database_url)
-
-    @cached_property
-    def connection(self):
-        return self.engine.connect()
-
-    @cached_property
-    def metadata(self):
-        metadata = platform.sqlalchemy.MetaData()
-        metadata.reflect(self.connection)
-        return metadata
-
-    @cached_property
-    def mapper(self):
-        # TODO: pass database_url
-        return platform.frictionless_formats.sql.SqlMapper(self.engine)
+            # Ensure records table
+            self.records = self.metadata.tables.get(RECORDS_IDENTIFIER)
+            if self.records is None:
+                self.records = sa.Table(
+                    RECORDS_IDENTIFIER,
+                    self.metadata,
+                    sa.Column("path", sa.Text, primary_key=True),
+                    sa.Column("type", sa.Text),
+                    sa.Column("updated", sa.DateTime),
+                    sa.Column("tableName", sa.Text, unique=True, nullable=True),
+                    sa.Column("resource", sa.Text),
+                    sa.Column("report", sa.Text),
+                )
+                self.metadata.create_all(conn, tables=[self.records])
 
     # General
 
     def query(self, query: str) -> IQueryData:
         sa = platform.sqlalchemy
-        result = self.connection.execute(sa.text(query))
-        rows = [row._asdict() for row in result]
-        header = list(result.keys())
-        return IQueryData(header=header, rows=rows)
+        with self.engine.begin() as conn:
+            result = conn.execute(sa.text(query))
+            header = list(result.keys())
+            rows = [row._asdict() for row in result]
+            return IQueryData(header=header, rows=rows)
 
     # Field
 
     def list_fields(self) -> List[IFieldItem]:
         sa = platform.sqlalchemy
         items: List[IFieldItem] = []
-        # TODO: write properly
-        result = self.connection.execute(
-            sa.text(
-                "SELECT path, tableName, json_extract(resource, '$.schema') as schema FROM _records WHERE type = 'table' ORDER BY tableName"
-            )
-        )
-        for row in result:
-            schema = Schema.from_descriptor(json.loads(row["schema"]))
-            for field in schema.fields:
-                items.append(
-                    IFieldItem(
-                        # TODO: review why it's not required
-                        name=field.name,  # type: ignore
-                        type=field.type,
-                        tableName=row["tableName"],
-                        tablePath=row["path"],
-                    )
+        with self.engine.begin() as conn:
+            result = conn.execute(
+                sa.select(
+                    self.records.c.path,
+                    self.records.c.tableName,
+                    sa.text("json_extract(resource, '$.schema')").label("schema"),
                 )
-
-        return items
+                .where(self.records.c.type == "table")
+                .order_by(self.records.c.tableName)
+            )
+            for row in result:
+                schema = Schema.from_descriptor(json.loads(row["schema"]))
+                for field in schema.fields:
+                    items.append(
+                        IFieldItem(
+                            # TODO: review why it's not required
+                            name=field.name,  # type: ignore
+                            type=field.type,
+                            tableName=row.tableName,
+                            tablePath=row.path,
+                        )
+                    )
+            return items
 
     # Record
 
     def create_record(self, resource: Resource, *, on_progress=None) -> IRecord:
-        with resource, self.connection.begin():
+        sa = platform.sqlalchemy
+        with resource, self.engine.begin() as conn:
             assert resource.path
             assert resource.name
             report = None
@@ -139,8 +133,7 @@ class Database:
                 # Remove existing table
                 existing_table = self.metadata.tables.get(table_name)
                 if existing_table is not None:
-                    existing_table.drop(self.connection)
-                    self.metadata.remove(existing_table)
+                    self.metadata.drop_all(conn, tables=[existing_table])
 
                 # Create new table
                 table = self.mapper.write_schema(
@@ -149,7 +142,7 @@ class Database:
                     with_metadata=True,
                 )
                 table.to_metadata(self.metadata)
-                table.create(self.connection)
+                self.metadata.create_all(conn, tables=[table])
 
                 # Write row
                 def on_row(row):
@@ -157,7 +150,7 @@ class Database:
                     cells = [row.row_number, row.valid] + cells
                     buffer.append(cells)
                     if len(buffer) > BUFFER_SIZE:
-                        self.connection.execute(table.insert().values(buffer))
+                        conn.execute(table.insert().values(buffer))
                         buffer.clear()
                     if on_progress:
                         on_progress(f"{resource.stats.rows} rows")
@@ -165,14 +158,14 @@ class Database:
                 # Validate/iterate
                 report = resource.validate(on_row=on_row)
                 if len(buffer):
-                    self.connection.execute(table.insert().values(buffer))
+                    conn.execute(table.insert().values(buffer))
 
             # Register resource
-            self.connection.execute(
-                self.records.delete(self.records.c.path == resource.path)
+            conn.execute(
+                sa.delete(self.records).where(self.records.c.path == resource.path)
             )
-            self.connection.execute(
-                self.records.insert().values(
+            conn.execute(
+                sa.insert(self.records).values(
                     path=resource.path,
                     type=resource.type,
                     tableName=table.name if table is not None else None,
@@ -182,62 +175,71 @@ class Database:
                 )
             )
 
-            # Return record
-            record = self.read_record(resource.path)
-            assert record
-            return record
+        # Return record
+        record = self.read_record(resource.path)
+        assert record
+        return record
 
     def delete_record(self, path: str) -> Optional[IRecord]:
+        sa = platform.sqlalchemy
         record = self.read_record(path)
         if record:
-            with self.connection.begin():
+            with self.engine.begin() as conn:
                 if record["tableName"]:
                     table = self.metadata.tables.get(record["tableName"])
                     if table is not None:
-                        table.drop(self.connection)
-                self.connection.execute(self.records.delete(self.records.c.path == path))
+                        self.metadata.drop_all(conn, tables=[table])
+                conn.execute(sa.delete(self.records).where(self.records.c.path == path))
             return record
 
     def list_records(self) -> List[IRecordItem]:
-        result = self.connection.execute(
-            self.records.select().with_only_columns(
-                [
+        sa = platform.sqlalchemy
+        with self.engine.begin() as conn:
+            result = conn.execute(
+                sa.select(
                     self.records.c.path,
                     self.records.c.type,
                     self.records.c.updated,
                     self.records.c.tableName,
-                ]
+                )
             )
-        )
-        items: List[IRecordItem] = []
-        for row in result:
-            item = IRecordItem(
-                path=row["path"],
-                type=row["type"],
-                updated=row["updated"].isoformat(),
-                tableName=row["tableName"],
-            )
-            items.append(item)
-        return items
+            items: List[IRecordItem] = []
+            for row in result:
+                item = IRecordItem(
+                    path=row.path,
+                    type=row.type,
+                    updated=row.updated.isoformat(),
+                    tableName=row.tableName,
+                )
+                items.append(item)
+            return items
 
     def move_record(self, source: str, target: str) -> str:
-        self.connection.execute(
-            self.records.update(self.records.c.path == source).values(path=target)
-        )
-        return target
+        sa = platform.sqlalchemy
+        with self.engine.begin() as conn:
+            conn.execute(
+                sa.update(self.records)
+                .where(self.records.c.path == source)
+                .values(path=target)
+            )
+            return target
 
     def read_record(self, path: str) -> Optional[IRecord]:
-        query = self.records.select(self.records.c.path == path)
-        row = self.connection.execute(query).first()
-        if row:
-            return IRecord(
-                path=row["path"],
-                type=row["type"],
-                updated=row["updated"].isoformat(),
-                tableName=row["tableName"],
-                resource=json.loads(row["resource"]),
-                report=json.loads(row["report"]),
-            )
+        sa = platform.sqlalchemy
+        with self.engine.begin() as conn:
+            row = conn.execute(
+                sa.select(self.records).where(self.records.c.path == path)
+            ).first()
+            if row:
+                return IRecord(
+                    # TODO: why it's a type error here but not in other places
+                    path=row.path,  # type: ignore
+                    type=row.type,  # type: ignore
+                    updated=row.updated.isoformat(),  # type: ignore
+                    tableName=row.tableName,  # type: ignore
+                    resource=json.loads(row.resource),  # type: ignore
+                    report=json.loads(row.report),  # type: ignore
+                )
 
     # TODO: implement
     def update_record(self, path: str):
@@ -250,7 +252,6 @@ class Database:
         schema = Schema.describe(result["rows"]).to_descriptor()
         return ITable(tableSchema=schema, header=result["header"], rows=result["rows"])
 
-    # TODO: rewrite
     def read_table(
         self,
         path: str,
@@ -259,16 +260,19 @@ class Database:
         limit: Optional[int] = None,
         offset: Optional[int] = None,
     ) -> ITable:
+        sa = platform.sqlalchemy
         record = self.read_record(path)
         assert record
         assert "tableName" in record
-        query = 'select * from "%s"' % record["tableName"]
+        assert record["tableName"] in self.metadata.tables
+        table = self.metadata.tables.get(record["tableName"])
+        query = sa.select(table)
         if valid is not None:
-            query = "%s where _rowValid = %s" % (query, valid)
+            query = query.where(table.c._rowValid == valid)
         if limit:
-            query = "%s limit %s" % (query, limit)
+            query = query.limit(limit)
             if offset:
-                query = "%s offset %s" % (query, offset)
-        data = self.query(query)
+                query = query.offset(offset)
+        data = self.query(str(query))
         schema = record["resource"]["schema"]
         return ITable(tableSchema=schema, header=data["header"], rows=data["rows"])
