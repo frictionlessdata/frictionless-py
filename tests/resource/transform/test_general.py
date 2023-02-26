8c67c589649292b5be162e2cd610354c19009fd7
roll <eskarev@gmail.com>
2023-02-26 11:11:53 +0000

    Deduplicated transform tests

--- tests/resource/transform/test_general.py
+++ tests/resource/transform/test_general.py
@@ -28,6 +28,33 @@ def test_resource_transform():
     ]
 
 
+def test_transform_resource_with_melt():
+    source = Resource("data/transform.csv")
+    pipeline = Pipeline(
+        steps=[
+            steps.table_normalize(),
+            steps.table_melt(field_name="id"),
+        ],
+    )
+    target = source.transform(pipeline)
+    assert isinstance(target, Resource)
+    assert target.schema.to_descriptor() == {
+        "fields": [
+            {"name": "id", "type": "integer"},
+            {"name": "variable", "type": "string"},
+            {"name": "value", "type": "any"},
+        ]
+    }
+    assert target.read_rows() == [
+        {"id": 1, "variable": "name", "value": "germany"},
+        {"id": 1, "variable": "population", "value": 83},
+        {"id": 2, "variable": "name", "value": "france"},
+        {"id": 2, "variable": "population", "value": 66},
+        {"id": 3, "variable": "name", "value": "spain"},
+        {"id": 3, "variable": "population", "value": 47},
+    ]
+
+
 def test_resource_transform_cell_set():
     source = Resource("data/transform.csv")
     pipeline = Pipeline.from_descriptor(
