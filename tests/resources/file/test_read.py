dc6948d8d61beda15e5bacb44193a2d3e827e10f
roll <eskarev@gmail.com>
2023-04-01 09:46:02 +0100

    Fixed tests

--- tests/resources/file/test_read.py
+++ tests/resources/file/test_read.py
@@ -1,9 +1,12 @@
+import pytest
+from frictionless import platform
 from frictionless.resources import FileResource
 
 
 # General
 
 
+@pytest.mark.skipif(platform.type == "windows", reason="Fix on Windows")
 def test_resource_read_file():
     resource = FileResource(path="data/article.md")
     assert resource.read_file() == b"# Article\n\nContents\n"
--- tests/resources/file/test_write.py
+++ tests/resources/file/test_write.py
@@ -1,9 +1,12 @@
+import pytest
+from frictionless import platform
 from frictionless.resources import FileResource
 
 
 # General
 
 
+@pytest.mark.skipif(platform.type == "windows", reason="Fix on Windows")
 def test_resource_write_text(tmpdir):
     source = FileResource(path="data/article.md")
     target = source.write_file(path=str(tmpdir.join("article.md")))
