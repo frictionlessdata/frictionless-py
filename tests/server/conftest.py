52e6a4515bbc8409d9f1f36e1681e0da0b6a9fce
roll <eskarev@gmail.com>
2023-04-13 12:20:38 +0100

    Fixed tests

--- tests/server/conftest.py
+++ tests/server/conftest.py
@@ -5,7 +5,7 @@ from frictionless.server import Server, Config
 
 @pytest.fixture(scope="session")
 def api_client(tmpdir_factory):
-    config = Config(basepath=tmpdir_factory.mktemp("server"))
+    config = Config(folder=tmpdir_factory.mktemp("server"))
     server = Server.create(config)
     client = TestClient(server)
     return client
@@ -13,7 +13,7 @@ def api_client(tmpdir_factory):
 
 @pytest.fixture(scope="session")
 def api_client_root():
-    config = Config(is_root=True)
+    config = Config()
     server = Server.create(config)
     client = TestClient(server)
     return client
