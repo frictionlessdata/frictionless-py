c96412d9159e268dc181d128a2e43944caf87018
roll <eskarev@gmail.com>
2023-04-27 16:27:21 +0100

    Rebased field/list on actions

--- frictionless/server/actions/field/list.py
+++ frictionless/server/actions/field/list.py
@@ -1,6 +1,9 @@
+import json
 from typing import List
 from pydantic import BaseModel
 from fastapi import Request
+from ....schema import Schema
+from ....platform import platform
 from ...project import Project, IFieldItem
 from ...router import router
 
@@ -14,7 +17,34 @@ class Result(BaseModel):
 
 
 @router.post("/field/list")
-def server_field_list(request: Request, props: Props) -> Result:
-    project: Project = request.app.get_project()
-    items = project.list_fields()
+def endpoint(request: Request, props: Props) -> Result:
+    return action(request.app.get_project(), props)
+
+
+def action(project: Project, props: Props) -> Result:
+    sa = platform.sqlalchemy
+    db = project.database
+    items: List[IFieldItem] = []
+    with db.engine.begin() as conn:
+        result = conn.execute(
+            sa.select(
+                db.records.c.path,
+                db.records.c.tableName,
+                sa.literal_column("json_extract(resource, '$.schema')").label("schema"),
+            )
+            .where(db.records.c.type == "table")
+            .order_by(db.records.c.tableName)
+        )
+        for row in result:
+            schema = Schema.from_descriptor(json.loads(row.schema))
+            for field in schema.fields:
+                items.append(
+                    IFieldItem(
+                        # TODO: review why field.name is not required
+                        name=field.name,  # type: ignore
+                        type=field.type,
+                        tableName=row.tableName,
+                        tablePath=row.path,
+                    )
+                )
     return Result(items=items)
