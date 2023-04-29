d77f82fbe00a4dfd2ba09047ff866379f23a14a0
roll <eskarev@gmail.com>
2023-04-29 14:16:15 +0100

    Recovered folder tests

--- frictionless/server/endpoints/folder/create.py
+++ frictionless/server/endpoints/folder/create.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from fastapi import Request
+from typing import Optional
 from ....exception import FrictionlessException
 from ...project import Project
 from ...router import router
@@ -7,6 +8,7 @@ from ...router import router
 
 class Props(BaseModel):
     path: str
+    folder: Optional[str] = None
 
 
 class Result(BaseModel):
@@ -22,7 +24,7 @@ def action(project: Project, props: Props) -> Result:
     fs = project.filesystem
 
     # Create
-    target = fs.get_fullpath(props.path)
+    target = fs.get_fullpath(props.folder, props.path)
     if target.exists():
         raise FrictionlessException("Folder already exists")
     target.mkdir(parents=True)
---
+++ tests/server/endpoints/folder/test_create.py
@@ -0,0 +1,22 @@
+import pytest
+from ...fixtures import folder1, not_secure
+
+
+# Create
+
+
+def test_server_folder_create(client):
+    path = client("/folder/create", path=folder1).path
+    assert path == folder1
+    assert client("/file/list").items == [
+        {"path": folder1, "type": "folder"},
+    ]
+
+
+@pytest.mark.parametrize("path", not_secure)
+def test_server_folder_create_security(client, path):
+    with pytest.raises(Exception):
+        client("/folder/create", path=path)
+    if path != "./":
+        with pytest.raises(Exception):
+            client("/folder/create", path=folder1, folder=path)
