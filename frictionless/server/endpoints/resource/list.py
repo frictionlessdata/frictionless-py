947c8a4fc4c99fdcb9941f45d42a18cda76977f5
roll <eskarev@gmail.com>
2023-05-10 16:28:38 +0100

    Bootstrapped resource/list

---
+++ frictionless/server/endpoints/resource/list.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import List
+from pydantic import BaseModel
+from fastapi import Request
+from ...project import Project
+from ...router import router
+from ...interfaces import IResourceItem
+
+
+class Props(BaseModel, extra="forbid"):
+    pass
+
+
+class Result(BaseModel, extra="forbid"):
+    items: List[IResourceItem]
+
+
+@router.post("/resource/list")
+def endpoint(request: Request, props: Props) -> Result:
+    return action(request.app.get_project(), props)
+
+
+# TODO: implement actual logic
+def action(project: Project, props: Props) -> Result:
+    return Result(
+        items=[
+            {"id": "id", "path": "path"},
+        ]
+    )
