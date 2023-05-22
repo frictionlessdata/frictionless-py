f8d9c77fb96b37ca80d28c26a6e03c9ca4512498
roll <eskarev@gmail.com>
2023-05-22 15:10:09 +0100

    Fixed server tests

--- tests/server/endpoints/file/test_copy.py
+++ tests/server/endpoints/file/test_copy.py
@@ -9,7 +9,7 @@ from ...fixtures import name1, name2, bytes1, folder1, folder2, not_secure
 
 def test_server_file_copy(client):
     client("/file/create", path=name1, bytes=bytes1)
-    path = client("/file/copy", source=name1, target=name2).path
+    path = client("/file/copy", path=name1, toPath=name2).path
     assert path == name2
     assert client("/file/read", path=name1).bytes == bytes1
     assert client("/file/read", path=name2).bytes == bytes1
@@ -22,7 +22,7 @@ def test_server_file_copy(client):
 def test_server_file_copy_deduplicate(client):
     name1copy = "name1 (copy1).txt"
     client("/file/create", path=name1, bytes=bytes1)
-    path = client("/file/copy", source=name1, deduplicate=True).path
+    path = client("/file/copy", path=name1, deduplicate=True).path
     assert path == name1copy
     assert client("/file/read", path=name1).bytes == bytes1
     assert client("/file/read", path=name1copy).bytes == bytes1
@@ -35,7 +35,7 @@ def test_server_file_copy_deduplicate(client):
 def test_server_file_copy_to_folder(client):
     client("/file/create", path=name1, bytes=bytes1)
     client("/folder/create", path=folder1)
-    path = client("/file/copy", source=name1, target=folder1).path
+    path = client("/file/copy", path=name1, toPath=folder1).path
     assert path == str(Path(folder1) / name1)
     assert client("/file/read", path=name1).bytes == bytes1
     assert client("/file/read", path=path).bytes == bytes1
@@ -52,7 +52,7 @@ def test_server_file_copy_from_folder_to_folder(client):
     client("/folder/create", path=folder1)
     client("/folder/create", path=folder2)
     client("/file/create", path=name1, bytes=bytes1, folder=folder1)
-    path = client("/file/copy", source=folder1, target=folder2).path
+    path = client("/file/copy", path=folder1, toPath=folder2).path
     assert path == str(Path(folder2) / folder1)
     assert client("/file/read", path=path1).bytes == bytes1
     assert client("/file/read", path=path2).bytes == bytes1
@@ -71,4 +71,4 @@ def test_server_file_copy_security(client, path):
     with pytest.raises(Exception):
         client("/file/copy", path=path)
     with pytest.raises(Exception):
-        client("/file/copy", path=name1, target=path)
+        client("/file/copy", path=name1, toPath=path)
--- tests/server/endpoints/file/test_move.py
+++ tests/server/endpoints/file/test_move.py
@@ -9,7 +9,7 @@ from ...fixtures import name1, name2, bytes1, folder1, folder2, not_secure
 
 def test_server_file_move(client):
     client("/file/create", path=name1, bytes=bytes1)
-    client("/file/move", source=name1, target=name2)
+    client("/file/move", path=name1, toPath=name2)
     assert client("/file/read", path=name2).bytes == bytes1
     assert client("/file/list").files == [
         models.File(path=name2, type="file"),
@@ -19,7 +19,7 @@ def test_server_file_move(client):
 def test_server_file_move_to_new_name(client):
     client("/folder/create", path=folder1)
     path1 = client("/file/create", path=name1, bytes=bytes1, folder=folder1).path
-    path2 = client("/file/move", source=path1, newName=name2).path
+    path2 = client("/file/move", path=path1, newName=name2).path
     assert client("/file/read", path=path2).bytes == bytes1
     assert client("/file/list").files == [
         models.File(path=folder1, type="folder"),
@@ -30,7 +30,7 @@ def test_server_file_move_to_new_name(client):
 def test_server_file_move_to_folder(client):
     client("/file/create", path=name1, bytes=bytes1)
     client("/folder/create", path=folder1)
-    path = client("/file/move", source=name1, target=folder1).path
+    path = client("/file/move", path=name1, toPath=folder1).path
     assert path == str(Path(folder1) / name1)
     assert client("/file/read", path=path).bytes == bytes1
     assert client("/file/list").files == [
@@ -42,7 +42,7 @@ def test_server_file_move_to_folder(client):
 def test_server_file_move_folder(client):
     client("/folder/create", path=folder1)
     client("/file/create", path=name1, bytes=bytes1, folder=folder1)
-    client("/file/move", source=folder1, target=folder2)
+    client("/file/move", path=folder1, toPath=folder2)
     assert client("/file/list").files == [
         models.File(path=folder2, type="folder"),
         models.File(path=str(Path(folder2) / name1), type="file"),
@@ -54,7 +54,7 @@ def test_server_file_move_folder_to_folder(client):
     client("/folder/create", path=folder1)
     client("/folder/create", path=folder2)
     client("/file/create", path=name1, bytes=bytes1, folder=folder1)
-    path = client("/file/move", source=folder1, target=folder2).path
+    path = client("/file/move", path=folder1, toPath=folder2).path
     assert path == str(Path(folder2) / folder1)
     assert client("/file/read", path=path2).bytes == bytes1
     assert client("/file/list").files == [
@@ -68,6 +68,6 @@ def test_server_file_move_folder_to_folder(client):
 def test_server_file_move_security(client, path):
     client("/file/create", path=name1, bytes=bytes1)
     with pytest.raises(Exception):
-        client("/file/move", source=path, target=folder1)
+        client("/file/move", path=path, toPath=folder1)
     with pytest.raises(Exception):
-        client("/file/move", source=name1, target=path)
+        client("/file/move", path=name1, toPath=path)
