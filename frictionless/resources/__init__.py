1a189c7cca9f82ed87cf6bf547b7ec954dd1ad5c
roll <eskarev@gmail.com>
2023-07-03 13:59:28 +0100

    Added top-level alls

--- frictionless/resources/__init__.py
+++ frictionless/resources/__init__.py
@@ -6,3 +6,32 @@ from .resource import *
 from .table import *
 from .text import *
 from .types import *
+
+__all__ = [
+    "ArticleResource"
+    "CatalogResource"
+    "ChartResource"
+    "ChecklistResource"
+    "DialectResource"
+    "DocumentResource"
+    "FileResource"
+    "ImageResource"
+    "InquiryResource"
+    "JsonResource"
+    "JsonschemaResource"
+    "MapResource"
+    "MetadataResource"
+    "PackageResource"
+    "PipelineResource"
+    "ReportResource"
+    "ResourceResource"
+    "SchemaResource"
+    "ScriptResource"
+    "TableResource"
+    "TextResource"
+    "ViewResource"
+    "Convertible"
+    "Extractable"
+    "Indexable"
+    "Transformable"
+]
