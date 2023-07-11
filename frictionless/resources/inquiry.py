483d8bf285096411d4c1954629a7990fa148ee02
roll <eskarev@gmail.com>
2023-07-11 13:00:28 +0100

    Fixed implementation

--- frictionless/resources/inquiry.py
+++ frictionless/resources/inquiry.py
@@ -1,31 +1,33 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Optional
+
+from .. import settings
 from ..exception import FrictionlessException
 from ..inquiry import Inquiry
 from ..report import Report
 from .metadata import MetadataResource
+
+if TYPE_CHECKING:
+    from .. import types
+    from ..checklist import Checklist
 
 
 class InquiryResource(MetadataResource[Inquiry]):
     datatype = "inquiry"
     dataclass = Inquiry
 
-    # Read
-
-    def read_metadata(self) -> Inquiry:
-        return self.dataclass.from_descriptor(
-            self.descriptor,
-            basepath=self.basepath,
-            dialect=self.dialect or None,
-            detector=self.detector or None,
-        )
-
     # Validate
 
-    def validate(  # type: ignore
+    def validate(
         self,
+        checklist: Optional[Checklist] = None,
         *,
+        name: Optional[str] = None,
+        on_row: Optional[types.ICallbackFunction] = None,
         parallel: bool = False,
+        limit_rows: Optional[int] = None,
+        limit_errors: int = settings.DEFAULT_LIMIT_ERRORS,
     ) -> Report:
         try:
             inquiry = self.read_metadata()
