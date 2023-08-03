4a08d16e2faec643c7af2bc3cdc87559c233ed50
shashi gharti <shashi.gharti@gmail.com>
2023-08-03 12:53:14 +0545

    Fix references when resource and field name is updated

--- tests/steps/resource/test_resource_update.py
+++ tests/steps/resource/test_resource_update.py
@@ -1,4 +1,4 @@
-from frictionless import Package, Pipeline, steps
+from frictionless import Package, Pipeline, Schema, steps, transform
 from frictionless.resources import TableResource
 
 # General
@@ -38,3 +38,56 @@ def test_step_resource_update_standalone_issue_1351():
     )
     target = source.transform(pipeline)
     assert target.title == "New title"
+
+
+def test_step_resource_update_referenced_as_foreign_key():
+    resource1 = TableResource(name="resource1", path="data/transform.csv")
+    resource2 = TableResource(name="resource2")
+    resource1.schema = Schema.from_descriptor(
+        {
+            "fields": [
+                {"name": "id", "type": "integer"},
+                {"name": "name", "type": "string"},
+                {"name": "population", "type": "integer"},
+            ],
+            "primaryKey": ["id"],
+        }
+    )
+    resource2.schema = Schema.from_descriptor(
+        {
+            "fields": [
+                {"name": "id", "type": "integer"},
+                {"name": "address", "type": "string"},
+                {"name": "country_name", "type": "integer"},
+            ],
+            "primaryKey": ["id"],
+            "foreignKeys": [
+                {
+                    "fields": ["country_name"],
+                    "reference": {"fields": ["id"], "resource": "resource1"},
+                }
+            ],
+        }
+    )
+    package = Package(name="test-package", resources=[resource1, resource2])
+    transform(
+        package,
+        steps=[
+            steps.resource_transform(
+                name="resource1",
+                steps=[
+                    steps.resource_update(
+                        name="resource1", descriptor={"name": "first-resource"}
+                    )
+                ],
+            )
+        ],
+    )
+    assert (
+        package.get_resource("first-resource").validate().flatten(["title", "message"])
+        == []
+    )
+    assert (
+        package.get_resource("resource2").schema.foreign_keys[0]["reference"]["resource"]
+        == "first-resource"
+    )
