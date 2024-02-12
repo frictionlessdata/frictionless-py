b3d8bf82801c2f564ee14f28af86570291952f54
Amélie Rondot <amelie.rondot@multi.coop>
2024-02-12 11:41:54 +0100

    Refacto: refactoring tests 2

--- frictionless/table/__spec__/test_header.py
+++ frictionless/table/__spec__/test_header.py
@@ -1,3 +1,5 @@
+import pytest
+
 import frictionless
 from frictionless import Schema, fields
 from frictionless.resources import TableResource
@@ -40,63 +42,37 @@ def test_missing_label():
         assert header.valid is False
 
 
-def test_missing_primary_key_label_with_shema_sync_issue_1633():
-    test_cases = [
-        {
-            "constraints": {"required": True},
-            "nb_errors": 1,
-            "types_errors_expected": ["missing-label"],
-        },
-        {
-            "constraints": {},
-            "nb_errors": 1,
-            "types_errors_expected": ["missing-label"],
-        },
-    ]
+@pytest.mark.parametrize(
+    "source, required, valid_report, nb_errors, types_errors_expected, header_case",
+    [
+        ([["B"], ["foo"]], {"required": True}, False, 1, ["missing-label"], True),
+        ([["B"], ["foo"]], {}, False, 1, ["missing-label"], True),
+        ([["a"], ["foo"]], {"required": True}, True, 0, [], False),  # Ignore header_case
+        ([["a"], ["foo"]], {}, True, 0, [], False),  # Ignore header_case
+    ],
+)
+def test_missing_primary_key_label_with_shema_sync_issue_1633(
+    source, required, valid_report, nb_errors, types_errors_expected, header_case
+):
+    schema_descriptor = {
+        "$schema": "https://frictionlessdata.io/schemas/table-schema.json",
+        "fields": [{"name": "A", "constraints": required}],
+        "primaryKey": ["A"],
+    }
 
-    for tc in test_cases:
-        schema_descriptor = {
-            "$schema": "https://frictionlessdata.io/schemas/table-schema.json",
-            "fields": [{"name": "A", "constraints": tc["constraints"]}],
-            "primaryKey": ["A"],
-        }
+    resource = TableResource(
+        source=source,
+        schema=Schema.from_descriptor(schema_descriptor),
+        detector=frictionless.Detector(schema_sync=True),
+        dialect=frictionless.Dialect(header_case=header_case),
+    )
 
-        resource = TableResource(
-            source=[["B"], ["foo"]],
-            schema=Schema.from_descriptor(schema_descriptor),
-            detector=frictionless.Detector(schema_sync=True),
-        )
+    report = frictionless.validate(resource)
 
-        report = frictionless.validate(resource)
+    assert report.valid == valid_report
+
+    if not report.valid:
         errors = report.tasks[0].errors
-
-        assert not report.valid
-        assert len(errors) == tc["nb_errors"]
-        for error, type_expected in zip(errors, tc["types_errors_expected"]):
+        assert len(errors) == nb_errors
+        for error, type_expected in zip(errors, types_errors_expected):
             assert error.type == type_expected
-
-    # Ignore header_case
-    test_cases = [
-        {
-            "constraints": {"required": True},
-        },
-        {
-            "constraints": {},
-        },
-    ]
-
-    for tc in test_cases:
-        schema_descriptor = {
-            "$schema": "https://frictionlessdata.io/schemas/table-schema.json",
-            "fields": [{"name": "A", "constraints": tc["constraints"]}],
-            "primaryKey": ["A"],
-        }
-
-        resource = TableResource(
-            source=[["a"], ["foo"]],
-            schema=Schema.from_descriptor(schema_descriptor),
-            detector=frictionless.Detector(schema_sync=True),
-            dialect=frictionless.Dialect(header_case=False),
-        )
-        report = frictionless.validate(resource)
-        assert report.valid
