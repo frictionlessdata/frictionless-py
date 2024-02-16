fb0fb0cb077778b13071149369d1a57d673a4caf
Leo Ghignone <leo.ghignone@utas.edu.au>
2024-02-16 11:35:26 +1100

    Add s3 adapter

--- frictionless/schemes/aws/__init__.py
+++ frictionless/schemes/aws/__init__.py
@@ -1,8 +1,10 @@
+from .adapter import AwsAdapter
 from .control import AwsControl
 from .loaders import S3Loader
 from .plugin import AwsPlugin
 
 __all__ = [
+    "AwsAdapter",
     "AwsControl",
     "AwsPlugin",
     "S3Loader",
