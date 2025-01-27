2d58bee8e33b42b3a18c03fdd6fd77230fe0b59a
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-01-27 09:41:05 +0100

    Remove unused functions

--- frictionless/vendors/stringcase.py
+++ frictionless/vendors/stringcase.py
@@ -26,7 +26,8 @@ SOFTWARE.
 
 # The "stringcase" package appears to unmaintained
 # https://github.com/okunishinishi/python-stringcase/issues/42
-# It has been vendored here so we can fix warnings that will eventually become errors
+# Relevant parts have been vendored here so we can fix warnings
+# that will eventually become errors
 # Code here was copied from the 1.2.0 version uploaded to PyPI
 # https://pypi.org/project/stringcase/1.2.0/
 
@@ -52,39 +53,6 @@ def camelcase(string: str):
     )
 
 
-def capitalcase(string: str):
-    """Convert string into capital case.
-    First letters will be uppercase.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Capital case string.
-
-    """
-
-    string = str(string)
-    if not string:
-        return string
-    return uppercase(string[0]) + string[1:]
-
-
-def constcase(string: str):
-    """Convert string into upper snake case.
-    Join punctuation with underscore and convert letters into uppercase.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Const cased string.
-
-    """
-
-    return uppercase(snakecase(string))
-
-
 def lowercase(string: str):
     """Convert string into lower case.
 
@@ -97,76 +65,6 @@ def lowercase(string: str):
     """
 
     return str(string).lower()
-
-
-def pascalcase(string: str):
-    """Convert string into pascal case.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Pascal case string.
-
-    """
-
-    return capitalcase(camelcase(string))
-
-
-def pathcase(string: str):
-    """Convert string into path case.
-    Join punctuation with slash.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Path cased string.
-
-    """
-    string = snakecase(string)
-    if not string:
-        return string
-    return re.sub(r"_", "/", string)
-
-
-def backslashcase(string: str):
-    """Convert string into spinal case.
-    Join punctuation with backslash.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Spinal cased string.
-
-    """
-    str1 = re.sub(r"_", r"\\", snakecase(string))
-
-    return str1
-    # return re.sub(r"\\n", "", str1))  # TODO: make regex fot \t ...
-
-
-def sentencecase(string: str):
-    """Convert string into sentence case.
-    First letter capped and each punctuations are joined with space.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Sentence cased string.
-
-    """
-    joiner = " "
-    string = re.sub(r"[\-_\.\s]", joiner, str(string))
-    if not string:
-        return string
-    return capitalcase(
-        trimcase(
-            re.sub(r"[A-Z]", lambda matched: joiner + lowercase(matched.group(0)), string)
-        )
-    )
 
 
 def snakecase(string: str):
@@ -189,65 +87,6 @@ def snakecase(string: str):
     )
 
 
-def spinalcase(string: str):
-    """Convert string into spinal case.
-    Join punctuation with hyphen.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Spinal cased string.
-
-    """
-
-    return re.sub(r"_", "-", snakecase(string))
-
-
-def dotcase(string: str):
-    """Convert string into dot case.
-    Join punctuation with dot.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Dot cased string.
-
-    """
-
-    return re.sub(r"_", ".", snakecase(string))
-
-
-def titlecase(string: str):
-    """Convert string into sentence case.
-    First letter capped while each punctuations is capitalsed
-    and joined with space.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Title cased string.
-
-    """
-
-    return " ".join([capitalcase(word) for word in snakecase(string).split("_")])
-
-
-def trimcase(string: str):
-    """Convert string into trimmed string.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Trimmed case string
-    """
-
-    return str(string).strip()
-
-
 def uppercase(string: str):
     """Convert string into upper case.
 
@@ -260,18 +99,3 @@ def uppercase(string: str):
     """
 
     return str(string).upper()
-
-
-def alphanumcase(string: str):
-    """Cuts all non-alphanumeric symbols,
-    i.e. cuts all expect except 0-9, a-z and A-Z.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: String with cutted non-alphanumeric symbols.
-
-    """
-    # return filter(str.isalnum, str(string))
-    return re.sub(r"\W+", "", string)
