2aadfe3b55242b02ea503989c1d92a9348e6062c
Pierre Camilleri <pierre.camilleri@multi.coop>
2026-04-07 10:53:09 +0200

    Linting

--- frictionless/__main__.py
+++ frictionless/__main__.py
@@ -1,8 +1,9 @@
 import warnings
 
+from .console import console
+
 warnings.filterwarnings("default", category=DeprecationWarning, module="frictionless")
 
-from .console import console
 
 if __name__ == "__main__":
     console(prog_name="frictionless")
